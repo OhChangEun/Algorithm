ce0f233a5b51645b4bc09ee4e4321c67fafaacac
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-07-29 20:48:30 +0900

    [Gold V] Title: AC, Time: 688 ms, Memory: 96932 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            String p = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+            String input = br.readLine();
+
+            Deque<Integer> deque = new ArrayDeque<>();
+            input = input.substring(1, input.length() - 1); // [ ] 제거
+
+            if (n > 0) {
+                String[] nums = input.split(",");
+                for (String num : nums) {
+                    deque.add(Integer.parseInt(num));
+                }
+            }
+
+            boolean isReversed = false;
+            boolean isError = false;
+
+            for (char cmd : p.toCharArray()) {
+                if (cmd == 'R') {
+                    isReversed = !isReversed;
+                } else if (cmd == 'D') {
+                    if (deque.isEmpty()) {
+                        isError = true;
+                        break;
+                    }
+                    if (isReversed) {
+                        deque.pollLast();
+                    } else {
+                        deque.pollFirst();
+                    }
+                }
+            }
+
+            if (isError) {
+                System.out.println("error");
+            } else {
+                StringBuilder sb = new StringBuilder();
+                sb.append("[");
+
+                while (!deque.isEmpty()) {
+                    sb.append(isReversed ? deque.pollLast() : deque.pollFirst());
+                    if (!deque.isEmpty()) sb.append(",");
+                }
+
+                sb.append("]");
+                System.out.println(sb.toString());
+            }
+        }
+    }
+}
