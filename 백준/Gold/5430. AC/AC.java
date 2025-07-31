55a80ad90ac53e3dacaa40452ac9fc1cc5475bf9
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-07-31 23:10:57 +0900

    [Gold V] Title: AC, Time: 672 ms, Memory: 97152 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -4,25 +4,26 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int T = Integer.parseInt(br.readLine());
 
-        while (T-- > 0) {
+        for (int i=0; i<T; i++) {
             String p = br.readLine();
             int n = Integer.parseInt(br.readLine());
             String input = br.readLine();
 
             Deque<Integer> deque = new ArrayDeque<>();
-            input = input.substring(1, input.length() - 1); // [ ] 제거
+            input = input.substring(1, input.length()-1);
 
-            if (n > 0) {
+            if (n>0) {
                 String[] nums = input.split(",");
-                for (String num : nums) {
+                for (String num: nums) {
                     deque.add(Integer.parseInt(num));
                 }
             }
 
-            boolean isReversed = false;
             boolean isError = false;
+            boolean isReversed = false;
 
             for (char cmd : p.toCharArray()) {
                 if (cmd == 'R') {
@@ -32,6 +33,7 @@ public class Main {
                         isError = true;
                         break;
                     }
+
                     if (isReversed) {
                         deque.pollLast();
                     } else {
@@ -50,7 +52,6 @@ public class Main {
                     sb.append(isReversed ? deque.pollLast() : deque.pollFirst());
                     if (!deque.isEmpty()) sb.append(",");
                 }
-
                 sb.append("]");
                 System.out.println(sb.toString());
             }
