035c0b53a2b1acb075014d1353af5560679ba0c9
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-08-04 22:01:52 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+   
+    public void bfs(int start, int[][] computers, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+        
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            
+            for (int i=0; i<computers.length; i++) {
+                if (curr != i && computers[curr][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+    
+        boolean[] visited = new boolean[n];
+        
+        for (int i=0; i<n; i++) {
+            if (!visited[i]) {
+                bfs(i, computers, visited);
+                answer++;
+            }
+        }
+         
+        return answer;
+    }
+}
