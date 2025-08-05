52e0a1968095121188256bd8740ca3c88ed2fc7f
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-08-06 08:45:14 +0900

    [level 3] Title: 야근 지수, Time: 121.51 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,26 +3,31 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());    
-        for (int i=0; i<works.length; i++) {
-       		maxHeap.add(works[i]); 
+       
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int workTime: works) {
+            maxHeap.add(workTime);
         }
+        
         while (n>0) {
-            int max = maxHeap.poll();
-            if (max-1 > 0)
-                maxHeap.add(max-1);
+           	int maxWorkTime = maxHeap.poll();
+           
+            // works가 0을 maxHeap에 넣으면 안되므로 
+            if (maxWorkTime > 1) {
+                maxHeap.add(maxWorkTime-1);
+            }
             n--;
             
             if (maxHeap.isEmpty()) {
-           		return 0;     
+                return 0;
             }
         }
-       	
+        
         while (!maxHeap.isEmpty()) {
             int num = maxHeap.poll();
-            answer += num * num;
-        } 
+            answer += (long)(num * num);
+        }
         
         return answer;
     }
