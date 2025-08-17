ffca665a81df92bb847fc46b866edeb265521cd3
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-08-17 23:12:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.80 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,9 +1,34 @@
 import java.util.*;
 
 class Solution {
-   
-    public int find(int[] parent, int x) { 
-   		if (parent[x] == x) return x;
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+     
+        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+        
+        int[] parent = new int[n];
+        for (int i=0; i<n; i++) {
+            parent[i] = i;
+        }
+        
+        int count = 0;
+        for (int[] cost: costs) {
+            int a = cost[0];
+            int b = cost[1];
+            int c = cost[2];
+            
+            if (find(parent, a) != find(parent, b)) {
+                union(parent, a, b);
+                answer += c;
+                count++;
+            }
+            
+            if (count == n-1) break;
+        }
+        return answer;
+    }
+    public int find(int[] parent, int x) {
+        if (parent[x] == x) return x;
         return parent[x] = find(parent, parent[x]);
     }
     public void union(int[] parent, int a, int b) {
@@ -12,44 +37,7 @@ class Solution {
         
         if (rootA < rootB) 
             parent[rootB] = rootA;
-        else 
+        else
             parent[rootA] = rootB;
-    }
-    
-    public int solution(int n, int[][] costs) {
-        int answer = 0;
-       
-        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
-     
-        int[] parent = new int[n];
-        for (int i=0; i<parent.length; i++) {
-            parent[i] = i;
-        }
-       
-        int count = 0;
-        for (int[] cost: costs) {
-            int a = cost[0];
-            int b = cost[1];
-            int c = cost[2]; // 비용(가중치)
-            
-            if (find(parent, a) != find(parent, b)) {
-                union(parent, a, b);
-                answer += c;
-                count++;
-            }
-            
-            if (count == n-1) 
-                break;
-        }
-        /*
-        for (int i=0; i<costs.length; i++) {
-            for (int j=0; j<costs[0].length; j++) {
-                System.out.print(costs[i][j]);
-            }
-            System.out.println();
-        }
-        */
-        
-        return answer;
     }
 }
