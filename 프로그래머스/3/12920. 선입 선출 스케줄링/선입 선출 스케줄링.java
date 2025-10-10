34aad68442d1a8d31cb3411148cacf6b485442a2
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-10-10 23:42:13 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 5.03 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -1,23 +1,16 @@
 class Solution {
     public int solution(int n, int[] cores) {
+        if (n <= cores.length) return n;
         
-        // 작업시간 0에 작업을 배치를 다 할 수 있으면 
-        if (n <= cores.length) {
-            return n;
-        }
-        
-        // 작업시간 0에 모든 작업 배치 
         n -= cores.length;
-       
-        // 작업시간 찾기 
-        int left = 0;
-        int right = n * 10000; // 최장 작업시간 
-        int targetTime = 0; // 작업에 걸리는 시간 
         
+        int left = 0;
+        int right = 10000 * n; 
+        int targetTime = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
-            int completedJobs = countCompletedJobs(mid, cores);
-
+           	int completedJobs = countCompletedJobs(mid, cores);
+            
             if (completedJobs >= n) {
                 targetTime = mid;
                 right = mid - 1;
@@ -25,25 +18,25 @@ class Solution {
                 left = mid + 1;
             }
         }
-     
-        int completedBeforeTarget = countCompletedJobs(targetTime - 1, cores);
+       
+        // System.out.println(targetTime);
+        int completedBeforeJobs = countCompletedJobs(targetTime - 1, cores);
+        // System.out.println(completedBeforeJobs);
         for (int i=0; i<cores.length; i++) {
             if (targetTime % cores[i] == 0) {
-                completedBeforeTarget++;
-            }
-            
-            if (completedBeforeTarget == n) {
-                return i + 1;
+            	completedBeforeJobs++;
+               	if (completedBeforeJobs == n) {
+                    return i + 1;
+                } 
             }
         }
         
         return -1;
     }
-    
     private int countCompletedJobs(int time, int[] cores) {
         int count = 0;
         for (int core: cores) {
-            count += time / core;
+            count += time / core; 
         }
         return count;
     }
