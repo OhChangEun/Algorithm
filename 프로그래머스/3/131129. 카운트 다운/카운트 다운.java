fe998fc8759981bdc51021aaace4904d36ce4dd9
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-10-11 00:48:10 +0900

    [level 3] Title: 카운트 다운, Time: 43.97 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,49 +1,46 @@
 class Solution {
     public int[] solution(int target) {
-        // target을 만들 수 있는 경우 
-        // dp[i][0]: i점을 만들 수 있는 최소 다트 수, 
-        // dp[i][1]: 싱글/불 수 
         int[][] dp = new int[target + 1][2];
-      
         for (int i=1; i<=target; i++) {
-            dp[i][0] = Integer.MAX_VALUE;
-            dp[i][1] = 0;
+            dp[i][0] = Integer.MAX_VALUE; 
         }
-      
-        // 찾으면서 기록하는 score: 1~target
+        
         for (int score = 1; score <= target; score++) {
-       		for (int single=1; single<=20; single++) {
-               	if (score >= single && dp[score - single][0] != Integer.MAX_VALUE) {
-                   	update(dp, score, dp[score - single][0] + 1, dp[score - single][1] + 1); 
-                } 
-            }
-			for (int num=1; num<=20; num++) {
+            for (int single = 1; single <= 20; single++) {
+                int prev = score - single;
+                if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
+                    update(dp, score, dp[prev][0] + 1, dp[prev][1] + 1);
+                }
+            } 
+            for (int num = 1; num <= 20; num++) {
                 int doubleScore = num * 2;
-                if (score >= doubleScore && dp[score - doubleScore][0] != Integer.MAX_VALUE) {
-                    update(dp, score, dp[score - doubleScore][0] + 1, dp[score - doubleScore][1]);
+                int prev = score - doubleScore;
+                if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
+                    update(dp, score, dp[prev][0] + 1, dp[prev][1]);
                 }
-            }            
-			for (int num=1; num<=20; num++) {
+            } 
+            for (int num = 1; num <= 20; num++) {
                 int tripleScore = num * 3;
-                if (score >= tripleScore && dp[score - tripleScore][0] != Integer.MAX_VALUE) {
-                    update(dp, score, dp[score - tripleScore][0] + 1, dp[score - tripleScore][1]);
+                int prev = score - tripleScore;
+                if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
+                    update(dp, score, dp[prev][0] + 1, dp[prev][1]);
                 }
-            }
-            
-            if (score >= 50 && dp[score - 50][0] != Integer.MAX_VALUE) {
-                update(dp, score, dp[score - 50][0] + 1, dp[score - 50][1] + 1);
+            } 
+            int prev = score - 50;
+            if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
+                update(dp, score, dp[prev][0] + 1, dp[prev][1] + 1);
             }
         }
         
         return dp[target];
     }
-    private void update(int[][] dp, int score, int darts, int singleOrBull) {
-        // 현재까지 알려진 다트 수보다 더 작다면 다트 수 갱신
-       	if (darts < dp[score][0]) { 
-            dp[score][0] = darts;
-            dp[score][1] = singleOrBull;
-        } else if (darts == dp[score][0] && singleOrBull > dp[score][1]) { // 현재까지 알려진 single/bull 방식으로 던진 다트 수 보다 더 크다면 다트 수 갱신 
-            dp[score][1] = singleOrBull;
+    
+    private void update(int[][] dp, int score, int total, int singleAndBull) {
+        if (total < dp[score][0]) {
+            dp[score][0] = total;
+            dp[score][1] = singleAndBull;
+        } else if (total == dp[score][0] && singleAndBull > dp[score][1]) {
+            dp[score][1] = singleAndBull; 
         }
     }
 }
