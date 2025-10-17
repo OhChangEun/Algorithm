5561d9c3dd3766c722b30087b7904da0db227d0f
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-10-17 20:45:25 +0900

    [level 3] Title: 카운트 다운, Time: 46.97 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,32 +1,34 @@
 class Solution {
     public int[] solution(int target) {
-        int[][] dp = new int[target + 1][2];
+        int dp[][] = new int[target + 1][2];
         for (int i=1; i<=target; i++) {
-            dp[i][0] = Integer.MAX_VALUE; 
+           	dp[i][0] = Integer.MAX_VALUE; 
         }
         
-        for (int score = 1; score <= target; score++) {
-            for (int single = 1; single <= 20; single++) {
+        for (int score=1; score<=target; score++) {
+           	for (int single = 1; single <= 20; single++) {
                 int prev = score - single;
-                if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
+           		if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
                     update(dp, score, dp[prev][0] + 1, dp[prev][1] + 1);
                 }
             } 
+            
             for (int num = 1; num <= 20; num++) {
-                int doubleScore = num * 2;
+                int doubleScore = num * 2; 
                 int prev = score - doubleScore;
                 if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
                     update(dp, score, dp[prev][0] + 1, dp[prev][1]);
                 }
-            } 
+            }
             for (int num = 1; num <= 20; num++) {
-                int tripleScore = num * 3;
+                int tripleScore = num * 3; 
                 int prev = score - tripleScore;
                 if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
                     update(dp, score, dp[prev][0] + 1, dp[prev][1]);
                 }
-            } 
-            int prev = score - 50;
+            }
+            
+            int prev = score - 50; 
             if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
                 update(dp, score, dp[prev][0] + 1, dp[prev][1] + 1);
             }
@@ -37,10 +39,10 @@ class Solution {
     
     private void update(int[][] dp, int score, int total, int singleAndBull) {
         if (total < dp[score][0]) {
-            dp[score][0] = total;
+           	dp[score][0] = total; 
             dp[score][1] = singleAndBull;
         } else if (total == dp[score][0] && singleAndBull > dp[score][1]) {
-            dp[score][1] = singleAndBull; 
+            dp[score][1] = singleAndBull;
         }
     }
 }
