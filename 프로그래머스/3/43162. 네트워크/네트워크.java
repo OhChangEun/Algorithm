3cb5e79cbdd0b80509353619b3e3457a0c174408
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-10-22 00:17:54 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,36 +1,32 @@
 import java.util.*;
 
 class Solution {
-    public void bfs(int start, boolean[] visited, int[][] computers) {
-       	Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        visited[start] = true;
+    boolean[] visited; 
+    int count = 0;
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[n]; 
+        for (int i=0; i<n; i++) {
+            if (!visited[i]) {
+                bfs(i, n, computers); 
+                count++;
+            }
+        }
+        return count;
+    }
+    private void bfs(int num, int n, int[][] computers) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(num);
+        visited[num] = true; 
         
         while (!queue.isEmpty()) {
             int curr = queue.poll();
-            
-            for (int i=0; i<computers.length; i++) {
-                if (i!=curr && computers[curr][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    queue.add(i); 
-                }
-            }
-        }
-    }
-    
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-       
-        boolean[] visited = new boolean[n];
-       
-        int num = 0;
-        for (int i=0; i<n; i++) {
-            if (!visited[i]) {
-                bfs(i, visited, computers);
-           		num++; 
-            }
-        }
         
-        return num;
+            for (int i=0; i<n; i++) {
+                if (i!=curr && computers[curr][i] == 1 && !visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+                } 
+            }
+        }
     }
 }
