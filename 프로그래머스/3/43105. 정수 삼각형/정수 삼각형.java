9f0c9af4fbb19eae05cedd3cf2c85ca368fbecd2
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-10-28 00:07:47 +0900

    [level 3] Title: 정수 삼각형, Time: 10.88 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,16 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-      
-        int size = triangle.length - 2;
-        
-        for (int i=size; i>=0; i--) {
+        int start = triangle.length - 2; 
+        for (int i=start; i>=0; i--) {
             for (int j=0; j<=i; j++) {
-            	triangle[i][j] += Math.max(triangle[i+1][j], triangle[i+1][j+1]);
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
             }
-        } 
+        }
         
         return triangle[0][0];
     }
