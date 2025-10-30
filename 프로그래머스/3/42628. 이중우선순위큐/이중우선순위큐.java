5f2eca087763beb1c2ee13b017f297888985cd7d
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-10-30 20:07:33 +0900

    [level 3] Title: 이중우선순위큐, Time: 69.12 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,38 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = {};
-       
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        
+      
         for (String operation: operations) {
-            String[] parts = operation.split(" "); // 공백으로 파싱
-            String op = parts[0];
+            String[] parts = operation.split(" ");
+			String op = parts[0];
             int num = Integer.parseInt(parts[1]);
-            
+           
             if (op.equals("I")) {
-                minHeap.add(num);
-                maxHeap.add(num);
+               	maxHeap.add(num); 
+               	minHeap.add(num); 
             } else if (op.equals("D")) {
-                if (minHeap.isEmpty()) continue;
-               
-                if (num == -1) {
-            		int min = minHeap.poll();
-                    maxHeap.remove(min);
-                } else if (num == 1) {
-            		int max = maxHeap.poll();
+               	if (minHeap.isEmpty()) continue;
+                
+                if (num == 1) {
+                    int max = maxHeap.poll();
                     minHeap.remove(max);
+                } else {
+                    int min = minHeap.poll();
+                    maxHeap.remove(min);
                 }
             }
         }
+        
         if (minHeap.isEmpty() && maxHeap.isEmpty()) {
-            answer = new int[] {0, 0};
+            return new int[]{0, 0};
         } else {
-            answer = new int[] {maxHeap.peek(), minHeap.peek()};
-            // System.out.println(minHeap);
+            return new int[]{maxHeap.poll(), minHeap.poll()};
         }
-            
-        return answer;
     }
 }
