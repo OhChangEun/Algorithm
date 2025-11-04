8eca524c9fef06232c07a5ec6c464e397c92c0e8
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-04 23:45:20 +0900

    [level 3] Title: 등굣길, Time: 0.64 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,41 +1,27 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        
-        int[][] dp = new int[n+1][m+1];
-      	
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=m; j++) {
-                dp[i][j] = -1;
-            }
+   		int[][] dp = new int[n + 1][m + 1];
+        boolean[][] isPuddle = new boolean[n + 1][m + 1];
+		int MAX  = 1_000_000_007;
+        // 웅덩이 위치 초기화 
+        for (int[] puddle: puddles) {
+            int x = puddle[0]; 
+            int y = puddle[1];
+            isPuddle[y][x] = true;
         }
         
-        // 물에 잠긴 지역 0으로 
-        for (int k=0; k<puddles.length; k++) {
-           	int waterX = puddles[k][1];
-            int waterY = puddles[k][0];
-        	
-            dp[waterX][waterY] = 0;
-        }
-
         for (int i=1; i<=n; i++) {
             for (int j=1; j<=m; j++) {
-               	if (i==1 && j==1) 
-                	dp[i][j] = 1;
-                else { 
-					if (dp[i][j] != 0) {
-                        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_007;
-                    } 
+                if (i == 1 && j == 1) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                if (!isPuddle[i][j]) {
+                   	dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MAX; 
                 }
             }
         }
-        
-//        for (int i=0; i<=n; i++) {
-//           for (int j=0; j<=m; j++) {
-//               System.out.print(dp[i][j] + " ");
-//           }
-//          System.out.println();
-//     }
         return dp[n][m];
     }
 }
