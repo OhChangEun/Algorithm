11f8db490971b98a328ab523e82f5374cb9ee784
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-13 19:32:42 +0900

    [level 3] Title: 최고의 집합, Time: 0.38 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,28 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int s) {
-        int[] answer = {};
+		if (n > s) return new int[] {-1};
+        // n == 3, 
+        // 8 / 3 = 2...2
+        // => {2, 3, 3} // 18
+       	// 1) s를 n으로 나누어서 n개의 크기를 가지는 배열에 그 몫을 분배한다. 
+        // 2) s를 n으로 나눈 나머지의 값에서 1씩 빼면서 그 배열에 분배한다. 
+        // 3) 오름차순으로 정렬하여 값을 반환한다. 
         
-        if (n > s) {
-            return new int[] {-1};
-        }
-        
-        // 집합 원소의 개수: n
-        // 모든 원소의 합: s 
-        answer = new int[n];
-        
+        int quotient = s / n; 
+        int remainder = s % n;
+      
+        int[] result = new int[n];
         for (int i=0; i<n; i++) {
-            answer[i] = s / n; // 11/3 = 3...2
-        }
-       
-        int value = s % n;
-        for (int i=0; i<value; i++) {
-            answer[i]++;
+            result[i] = quotient;
         }
         
-        Arrays.sort(answer);
+        for (int i=0; i<remainder; i++) {
+            result[n-1-i] += 1;
+        }
         
-        return answer;
+        return result;
     }
 }
