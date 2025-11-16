1d97aaa4a642a3182711d051d41840c809803d49
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-16 12:48:38 +0900

    [level 3] Title: 기지국 설치, Time: 0.55 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,24 +1,24 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        int answer = 0;
-        int range = 2*w + 1;
-       	int start = 1; // 현재 아파트 번호(1-based)
+        int answer = 0; 
+        int start = 1;
+        int range = 2 * w + 1;
         
-        for (int station: stations) { // 모든 기지국에 대해 
-           	int left = station - w; // 기지국 왼쪽 좌표 
-            if (start < left) {
-                int gap = left-start;
-                answer += (gap + range - 1) / range; // 올림 계산 
+        for (int station: stations) {
+            int left = station - w;
+           
+            int gap = left - start;
+            if (gap > 0) {
+            	answer += (gap + range - 1) / range;
             }
-            start = station + w + 1; // 다음 지점(기지국 오른쪽 좌표)으로 이동 
+            start = station + w + 1;
         }
-       
-        // 기지국이 중간에서 끝나는 경우
+
         if (start <= n) {
-            int gap = n - start + 1;
+           	int gap = n - start + 1;
             answer += (gap + range - 1) / range;
         }
-        
+
         return answer;
     }
 }
