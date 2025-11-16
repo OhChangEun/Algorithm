b7f708492385ca9fca64b80b42a4a1b0b1ea2a8f
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-16 11:47:06 +0900

    [level 3] Title: 단속카메라, Time: 9.84 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,30 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        // 고속도로 나간 지점 기준 오름차순
-        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
-        
-        int count = 0;
-        int lastCameraPos = -30001;
-        for (int i=0; i<routes.length; i++) {
-            int inPos = routes[i][0]; // 고속도로 진입 지점
-            int outPos = routes[i][1]; // 고속도로 진출 지점
-            
-            // 고속 도로 진입 지점이 최근 카메라 위치보다 뒤라면,
-            // 카메라 설치 
-            if (inPos > lastCameraPos) {
-               	count++;
-                lastCameraPos = outPos;
-            }
+   		Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+      
+        /*
+       	for(int[] route: routes) {
+        	System.out.println(route[0] + ", " + route[1]);
         } 
-       	/* 
-        for (int i=0; i<routes.length; i++) {
-        	for (int j=0; j<routes[i].length; j++) {
-                System.out.print(routes[i][j] + " ");
-            }    
-            System.out.println();
+        */
+       
+        int cameraCnt = 0;
+        int lastCameraPos = -30001;
+        for (int[] route: routes) {
+           	int start = route[0]; 
+           	int end = route[1]; 
+            if (lastCameraPos < start) {
+               	cameraCnt++;
+               	lastCameraPos = end; 
+            }
         }
-       	*/ 
-        return count;
+        
+        return cameraCnt;
     }
 }
