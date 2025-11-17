f1e72c4bca1e6be33e01946a7cf95e6f52f29b03
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-17 15:31:23 +0900

    [level 3] Title: 불량 사용자, Time: 74.93 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
 
 ### 성능 요약
 
-메모리: 113 MB, 시간: 69.51 ms
+메모리: 144 MB, 시간: 74.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 19일 23:20:58
+2025년 11월 17일 15:31:21
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,20 +1,21 @@
 import java.util.*;
 
 class Solution {
-    Set<Set<String>> result = new HashSet<>();
+  	Set<Set<String>> result = new HashSet<>(); 
+    
     public int solution(String[] user_id, String[] banned_id) {
-       	backtrack(user_id, banned_id, 0, new HashSet<>()); 
+   		backtrack(user_id, banned_id, 0, new HashSet<>());
         return result.size();
     }
     
-    private void backtrack(String[] user_id, String[] banned_id, int depth, Set<String> banned) {
+    public void backtrack(String[] user_id, String[] banned_id, int depth, Set<String> banned) {
         if (depth == banned_id.length) {
            	result.add(new HashSet<>(banned)); 
             return;
         }
         
         for (String user: user_id) {
-           	if (banned.contains(user)) continue;
+            if (banned.contains(user)) continue;
             
             if (canBan(user, banned_id[depth])) {
                 banned.add(user);
@@ -24,14 +25,13 @@ class Solution {
         }
     }
     
-    private boolean canBan(String user, String ban) {
-        if (user.length() != ban.length()) return false; 
-       	for (int i=0; i<user.length(); i++) {
-        	if (ban.charAt(i) != '*' && user.charAt(i) != ban.charAt(i)) {
-               	return false; 
+    public boolean canBan(String str, String ban) {
+        if (str.length() != ban.length()) return false; 
+        for (int i=0; i<str.length(); i++) {
+            if (ban.charAt(i) != '*' && str.charAt(i) != ban.charAt(i)) {
+                return false;
             }
-        } 
+        }
         return true;
     }
-    
 }
