c5af3263c4e18e07f8ad4bd6f616f70dd951bf15
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-18 12:12:21 +0900

    [level 3] Title: 가장 먼 노드, Time: 52.09 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,88 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public int bfs(int start, List<List<Integer>> graph, boolean[] visited, int[] distance) {
-   		Queue<Integer> queue = new LinkedList<>();	
-        queue.offer(start);
-        visited[start] = true;
-      	distance[1] = 1; 
-       
-        int count = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-       		count = size;	
-            // System.out.print(distance[curr] + " "); 
-            for (int i=0; i<size; i++) {
-            	int curr = queue.poll();
-                for (int next: graph.get(curr)) {
-                    if (!visited[next]) {
-                        visited[next] = true;
-                        queue.offer(next);
-                        distance[next] = distance[curr] + 1;
-                    }
-                } 
-            }
-            /*
-            for (int i=0; i < graph.length; i++) {
-                if (graph[curr][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    queue.offer(i);
-                    distance[i] = distance[curr] + 1;
-                }
-            }
-            */
-        }
-        return count;
-    } 
-    
     public int solution(int n, int[][] edge) {
-      
-        /*
-        int[][] graph = new int[n+1][n+1];
-        
-        for (int i=0; i<edge.length; i++) {
-       		int start = edge[i][0]; 
-       		int end = edge[i][1];
-            
-            graph[start][end] = 1;
-            graph[end][start] = 1;
-        }
-        */
+        int answer = 0;
+       
         List<List<Integer>> graph = new ArrayList<>();
-        
         for (int i=0; i<=n; i++) {
             graph.add(new ArrayList<>());
         }
-       
+        // System.out.println(graph);
         for (int[] e: edge) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
+            int u = e[0];
+            int v = e[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        // System.out.println(graph);
+       
+        int start = 1;
+        int[] dist = bfs(n, start, graph);
+
+        int max = 0;
+        for (int d: dist) {
+            max = Math.max(d, max);
         }
         
-        
-    	boolean[] visited = new boolean[n+1]; 
-       	int[] distance = new int[n+1];
         int count = 0;
-        for (int i=1; i<=n; i++) {
-            if (!visited[i]) {
-                count = bfs(1, graph, visited, distance);
-            }
+        for (int d: dist) {
+            if (d == max) count++;
         }
-      
-        /*
-        int count = 0;
-        int max = distance[0];
-        for (int d: distance) {
-            if (max < d) 
-       			max = d;        
-        }
-        
-        for (int d: distance) {
-            if (max == d) {
-                count++;
-            }
-        }
-        */
+       
         return count;
+    }
+    public int[] bfs(int n, int start, List<List<Integer>> graph) {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, -1); 
+        
+        Queue<Integer> queue = new LinkedList<>();
+        dist[start] = 0;
+        queue.offer(start);
+        
+        while(!queue.isEmpty()) {
+            int curr = queue.poll();
+        
+            // System.out.println(curr);
+            for (int next: graph.get(curr)) {
+                if (dist[next] == -1) {
+                    dist[next] = dist[curr] + 1;
+                    queue.offer(next);
+                }
+            }
+        }
+       
+        return dist;
     }
 }
