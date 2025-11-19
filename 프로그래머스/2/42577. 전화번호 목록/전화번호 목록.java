241695002fe9a6f02907fc2d7d91a47be9bc49f5
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-19 21:54:58 +0900

    [level 2] Title: 전화번호 목록, Time: 387.79 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-       
-        Arrays.sort(phone_book);
+   		Arrays.sort(phone_book);
+      
         
-        for (int i=1; i<phone_book.length; i++) {
-            if (phone_book[i].startsWith(phone_book[i-1]))
-                return false;
+        int n = phone_book.length;
+        for (int i=0; i<n-1; i++) {
+           	String a = phone_book[i];
+            String b = phone_book[i + 1];
+           	
+            if (b.startsWith(a)) return false;
         }
         
         return true;
