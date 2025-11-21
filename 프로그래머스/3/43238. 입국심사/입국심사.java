7e825f4d5eeea4ff2aee83bb6acd5c6ce9b42a35
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-21 17:19:16 +0900

    [level 3] Title: 입국심사, Time: 60.16 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,27 +1,35 @@
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        long MAX_MAN = 1_000_000_000;
-        long MAX_WORK_TIME = 1_000_000_000;
+        long max = 0;
+        for (int time: times) {
+            max = Math.max(max, time);
+        }
         
-        long left = 1; // 기다리는 시간 최소값
-        long right = MAX_MAN * MAX_WORK_TIME; // 입국 심사 최악의 기다리는 시간 
+        long answer = 0;
+        long left = 0;
+        long right = n * max;
         while (left <= right) {
-        	long mid = (left + right) / 2;  
-           
-            long sum = 0;
-            for (int time: times) {
-               	sum += mid / time;
-            }
-            
-            if (sum >= n) {
-                answer = mid;
-                right = mid - 1;
+            long mid = (left + right) / 2;
+            if (canFinish(times, mid, n)) { 
+    			answer = mid;   
+      			right = mid - 1;         
             } else {
                 left = mid + 1;
             }
         }
         
         return answer;
+    }
+    
+    public boolean canFinish(int[] times, long mid, int n) {
+        long total = 0;
+        for (long time: times) {
+            total += mid / time;
+        }
+        
+        if (total >= n) {
+           	return true; 
+        }
+        return false;
     }
 }
