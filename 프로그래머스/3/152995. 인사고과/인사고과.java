a85c271dad6e2cbc29ef70e09f5ac78a759f2252
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-27 17:18:31 +0900

    [level 3] Title: 인사고과, Time: 113.23 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,42 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] scores) {
-        int answer = 0;
-       
-        int myScore1 = scores[0][0];
-        int myScore2 = scores[0][1];
-      	int size = scores.length;
-        
-        // 근무 태도 점수 내림차순, 동료 평가 오름차순 정렬
+   		int[] myScore = Arrays.copyOf(scores[0], 2); 
+      
         Arrays.sort(scores, (a, b) -> {
-            if (a[0] == b[0]) return a[1] - b[1];
+          	if (a[0] == b[0]) return a[1] - b[1];
             return b[0] - a[0];
         });
-     
-        // 다른 사원보다 두 점수가 모두 낮은 경우 인센티브 제외
-        int max = scores[0][1];  
-        for (int i=1; i<size; i++) {
-            if (scores[i][1] < max) {
-                // 완호 인지 확인 
-                if (scores[i][0] == myScore1 && scores[i][1] == myScore2) {
-                    return -1;
-                } else {
-                    scores[i][0] = -1;
-                    scores[i][1] = -1;
-                }
+        
+        int maxScore2 = scores[0][1];
+        for (int i=1; i<scores.length; i++) {
+			if (scores[i][1] < maxScore2) {
+                if (scores[i][0] == myScore[0] && scores[i][1] == myScore[1]) return -1;
+           		scores[i][0] = -1; 
+                scores[i][1] = -1;
             } else {
-                max = scores[i][1];
+               	maxScore2 = scores[i][1]; 
             }
         }
-        
-        // 완호 등수 매기기
+       	
         int rate = 1;
-        for (int i=0; i<size; i++) {
-            if (scores[i][0] + scores[i][1] > myScore1 + myScore2) {
-                rate++;
-            }
-        }
-        
+       	for (int[] score: scores) {
+           	int myTotalScore = myScore[0] + myScore[1]; 
+           	int totalScore = score[0] + score[1]; 
+            if (myTotalScore < totalScore) rate++;
+        } 
         return rate;
     }
 }
