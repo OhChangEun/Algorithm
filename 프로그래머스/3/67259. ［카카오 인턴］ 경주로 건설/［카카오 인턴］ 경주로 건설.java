28b7fcdee20f91802a698d892e062fe1026329f5
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-27 14:56:16 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 8.72 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,65 +1,68 @@
 import java.util.*;
 
 class Solution {
-    class Node {
-        int x, y, dir, cost;
-        public Node (int x, int y, int dir, int cost) {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
-            this.cost = cost;
+    class Node { 
+        int x, y, dir, cost; 
+        public Node (int x, int y, int dir, int cost) { 
+            this.x = x; 
+            this.y = y; 
+            this.dir = dir; 
+            this.cost = cost; 
         }
-    }
-    
-    int[] dx = { 0, 0, -1, 1 }; // 아래, 위, 왼쪽, 오른쪽
-    int[] dy = { 1, -1, 0, 0 };
+    } 
+    // 0: 상
+    // 1: 하
+    // 2: 좌
+    // 3: 우
+    int[] dx = new int[] {0, 0, -1, 1};
+    int[] dy = new int[] {-1, 1, 0, 0};
+    // 각 방향에 따른 최소값
+    int[][][] cost;
     
     public int solution(int[][] board) {
-   		int n = board.length;
-        int[][][] cost = new int[n][n][4];
-    	for (int i=0; i<n; i++) {
+        int n = board.length;
+        cost = new int[n][n][4]; 
+        for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 Arrays.fill(cost[i][j], Integer.MAX_VALUE);
             }
-        }
+        } 
         
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0, 0, 0, 0)); // 아래 방향 
-        q.offer(new Node(0, 0, 3, 0)); // 오른쪽 방향 
-    
-   		cost[0][0][0] = 0;
-        cost[0][0][3] = 0;
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.offer(new Node(0, 0, 1, 0));
+        queue.offer(new Node(0, 0, 3, 0));
        
-        int answer = Integer.MAX_VALUE;
-        while (!q.isEmpty()) {
-        	Node curr = q.poll();
-            
+        // 오른쪽, 아래 방향으로 시작
+        cost[0][0][1] = 0; 
+        cost[0][0][3] = 0; 
+
+        int minCost = Integer.MAX_VALUE;
+        while (!queue.isEmpty()) {
+            Node curr = queue.poll();
+            // 도착지에 도착하면 방향에 상관없이 x,y 좌표를 통해 최소 비용 구하기  
             if (curr.x == n-1 && curr.y == n-1) {
-               	answer = Math.min(answer, curr.cost);
-                continue;
+                minCost = Math.min(minCost, curr.cost);
             }
-            
             for (int dir=0; dir<4; dir++) {
-                int nx = curr.x + dx[dir];
-                int ny = curr.y + dy[dir];
+                int nx = curr.x + dx[dir]; 
+                int ny = curr.y + dy[dir]; 
+               
+                if (nx < 0 || ny < 0 || nx > n-1 || ny > n-1) continue; // 막다른 길 
+                if (board[nx][ny] == 1) continue; // 벽
                 
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-                if (board[ny][nx] == 1) continue;
-                
-                int newCost = curr.cost;
-                if (curr.dir == dir) { // 들어온 방향이 나가는 방향과 같은 경우 
-                    newCost += 100;
+                int newCost = 0;
+                if (dir == curr.dir) {
+                    newCost = curr.cost + 100;
                 } else {
-                   	newCost += 600; 
+                    newCost = curr.cost + 600; 
                 }
-                
-                if (newCost < cost[ny][nx][dir]) {
-                    cost[ny][nx][dir] = newCost;
-                    q.offer(new Node(nx, ny, dir, newCost));
+               
+                if (newCost < cost[nx][ny][dir]) {
+                    cost[nx][ny][dir] = newCost;
+                    queue.offer(new Node(nx, ny, dir, newCost));
                 }
-            }
+            } 
         }
-   
-        return answer;
+        return minCost;
     }
 }
