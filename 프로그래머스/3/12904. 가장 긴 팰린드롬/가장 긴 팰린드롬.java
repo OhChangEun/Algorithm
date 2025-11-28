961284a084a16889a09f34cc154b529840b974ae
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-11-28 15:52:15 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 14.85 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,22 +1,21 @@
+import java.util.*;
+
 class Solution
 {
-    public int getPalindromeLength(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+    public int getPalindromeLength(String str, int left, int right) {
+        while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
             left--;
             right++;
-        }
-   		return right - left - 1; 
-    }
+        } 
+        return right - left - 1;
+    } 
     public int solution(String s)
     {
-    	int size = s.length(); 
-        
-        int maxLen = 1;
-        for (int i=0; i<size; i++) {
-           	maxLen = Math.max(maxLen, getPalindromeLength(s, i, i));
-            maxLen = Math.max(maxLen, getPalindromeLength(s, i, i+1));
-        }
-        
-        return maxLen;
+        int maxLength = 1; 
+        for (int i=0; i<s.length(); i++) {
+            maxLength = Math.max(maxLength, getPalindromeLength(s, i, i)); 
+            maxLength = Math.max(maxLength, getPalindromeLength(s, i, i + 1)); 
+        } 
+        return maxLength;
     }
 }
