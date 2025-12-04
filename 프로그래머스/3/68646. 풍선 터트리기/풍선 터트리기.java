90134a0c5d6f586104367b4bb443713358c7cf6f
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-12-04 20:31:31 +0900

    [level 3] Title: 풍선 터트리기, Time: 32.95 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,31 +1,33 @@
 class Solution {
     public int solution(int[] a) {
-        int n = a.length;
+        int n = a.length; 
         int[] prefixMin = new int[n];
-        int[] suffixMin = new int[n];
+        int[] postfixMin = new int[n];
         
         prefixMin[0] = Integer.MAX_VALUE;
-        for (int i=1; i<n; i++) {
-           	prefixMin[i] = Math.min(prefixMin[i-1], a[i-1]);
+        for (int i = 1; i < n; i++) {
+            prefixMin[i] = Math.min(prefixMin[i - 1], a[i - 1]); 
         }
         
-        suffixMin[n-1] = Integer.MAX_VALUE;
-        for (int i=n-2; i>=0; i--) {
-            suffixMin[i] = Math.min(suffixMin[i+1], a[i+1]);
+        postfixMin[n - 1] = Integer.MAX_VALUE;
+        for (int i = n - 2; i >= 0; i--) {
+            postfixMin[i] = Math.min(postfixMin[i + 1], a[i + 1]); 
         }
-       
-        int count = 0;
-        for (int i=0; i<n; i++) {
-            boolean leftMin = prefixMin[i] < a[i];
-            boolean rightMin = suffixMin[i] < a[i];
-            
-            if (leftMin && rightMin) {
-                continue;
-            }
-            count++;
-        }
-		// System.out.println(prefixMin[2] + " " + suffixMin[2]);
         
-        return count;
+        int result = 0; 
+        for (int i = 0; i < n; i++) {
+            if (a[i] > prefixMin[i] && a[i] > postfixMin[i]) continue;
+            result++;
+        }
+        /*
+        for (int num: prefixMin) {
+            System.out.print(num + " "); 
+        }
+        System.out.println(); 
+        for (int num: postfixMin) {
+            System.out.print(num + " "); 
+        }
+        */ 
+        return result;
     }
 }
