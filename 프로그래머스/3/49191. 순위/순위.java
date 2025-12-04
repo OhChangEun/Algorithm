5f3bc9407386a2c2a50133ccde639ee4d8ea6ddc
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-12-04 22:05:17 +0900

    [level 3] Title: 순위, Time: 15.98 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,52 +1,54 @@
 import java.util.*;
 
 class Solution {
-    List<List<Integer>> winGraph = new ArrayList<>();
-    List<List<Integer>> loseGraph = new ArrayList<>();
-    
     public int solution(int n, int[][] results) {
+   		List<List<Integer>> winGraph = new ArrayList<>();	
+   		List<List<Integer>> loseGraph = new ArrayList<>();	
+      
         for (int i=0; i<=n; i++) {
-            winGraph.add(new ArrayList<>());
-            loseGraph.add(new ArrayList<>());
+           	winGraph.add(new ArrayList<>()); 
+           	loseGraph.add(new ArrayList<>()); 
         }
         
         for (int[] result: results) {
-            int win = result[0];
-            int lose = result[1];
-            
-            winGraph.get(win).add(lose);
-            loseGraph.get(lose).add(win);
-        }
-
-        int answer = 0;
+           	int u = result[0]; 
+           	int v = result[1]; 
+        	winGraph.get(u).add(v); 
+        	loseGraph.get(v).add(u); 
+        } 
+		
+        int result = 0;
         for (int i=1; i<=n; i++) {
-            int winCount = bfs(i, winGraph, n);
-            int loseCount = bfs(i, loseGraph, n);
-            
-            if (winCount + loseCount == n-1) {
-                answer++;
-            }
+            int winCount = bfs(n, winGraph, i);
+            int loseCount = bfs(n, loseGraph, i);
+           
+            int totalCount = winCount + loseCount;
+            if (totalCount == n - 1) 
+                result++;
         }
         
-        return answer;
+        return result;
     }
-    public int bfs(int start, List<List<Integer>> graph, int n) {
-        boolean[] visited = new boolean[n+1];
-        Queue<Integer> queue = new LinkedList<>();
+    public int bfs(int n, List<List<Integer>> graph, int start) {
+        Queue<Integer> queue = new LinkedList<>(); 
+   		boolean[] visited = new boolean[n + 1]; 
+        
         queue.offer(start);
-		visited[start] = true;
-       
+   		visited[start] = true; 
+ 
+        // bfs 시 방문하는 노드 수
         int count = 0;
         while (!queue.isEmpty()) {
             int curr = queue.poll();
             for (int next: graph.get(curr)) {
                 if (!visited[next]) {
-                    visited[next] = true;
                     queue.offer(next);
+                    visited[next] = true;
                     count++;
                 }
             }
         }
+        
         return count;
     }
 }
