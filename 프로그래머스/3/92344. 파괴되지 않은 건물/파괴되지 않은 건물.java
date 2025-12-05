49e5100ff4556975f9ebe7f9fc14f0f9fb437da8
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-12-05 23:49:49 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 49.89 ms, Memory: 221 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,63 +1,45 @@
 class Solution {
-   	int N, M; 
-    int[][] diff;
     public int solution(int[][] board, int[][] skill) {
-        N = board.length;
-        M = board[0].length;
-      
-        diff = new int[N + 1][M + 1];
-      	
-        for (int[] s: skill) {
-            int type = s[0];
-            int r1 = s[1], c1 = s[2];
-            int r2 = s[3], c2 = s[4];
-            int degree = s[5];
-            
-            degree = type == 1 ? -degree : degree;
-            
-            diff[r1][c1] += degree;
-            diff[r1][c2 + 1] -= degree;
-            diff[r2 + 1][c1] -= degree;
-            diff[r2 + 1][c2 + 1] += degree;
-        }
-                   
-        // 열 누적합 
-        for (int i=0; i<N; i++) {
-            for (int j=1; j<M; j++) { 
-				diff[i][j] += diff[i][j-1];	
-            }
-        }
-       
-        // 행 누적합 
-        for (int j=0; j<M; j++) {
-            for (int i=1; i<N; i++) { 
-				diff[i][j] += diff[i-1][j];	
-            }
-        }
-
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) { 
-				board[i][j] += diff[i][j];	
-            }
-        }
-       
-        int count = 0;
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) { 
-				if (board[i][j] > 0) {
-                    count++;
-                }	
-            }
-        }
-        /*
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) { 
-                System.out.print(board[i][j] + " ");
-			}
-            System.out.println();
-        }
-        */
+        int n = board.length;
+        int m = board[0].length;
         
-        return count;
+        int[][] sum = new int[n + 1][m + 1];
+        
+        for (int[] s: skill) {
+            int type = s[0]; 
+            int r1 = s[1], c1 = s[2]; 
+            int r2 = s[3], c2 = s[4]; 
+            int degree = s[5]; 
+     
+            degree = type == 1 ? -degree : degree; 
+            
+            sum[r1][c1] += degree; 
+            sum[r1][c2 + 1] -= degree;
+            sum[r2 + 1][c1] -= degree;
+            sum[r2 + 1][c2 + 1] += degree;
+        }
+        
+        for (int row=0; row<n; row++) {
+            for (int col=1; col<m; col++) {
+                sum[row][col] += sum[row][col - 1];
+            } 
+        }
+        
+        for (int col=0; col<m; col++) {
+            for (int row=1; row<n; row++) {
+                sum[row][col] += sum[row - 1][col];
+            } 
+        }
+        
+        int result = 0;
+        for (int row=0; row<n; row++) {
+            for (int col=0; col<m; col++) {
+                int value = board[row][col] + sum[row][col];
+                if (value > 0) 
+                    result++; 
+            }
+        }
+        
+        return result;
     }
 }
