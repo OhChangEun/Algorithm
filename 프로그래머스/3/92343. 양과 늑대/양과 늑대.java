37cea9821f63f2615ada0dac67c566a1d152be7a
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-12-08 23:04:14 +0900

    [level 3] Title: 양과 늑대, Time: 10.24 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,45 +1,46 @@
 import java.util.*;
 
 class Solution {
-    int result = 0;
+    int maxSheep = 0;
+   
     public int solution(int[] info, int[][] edges) {
    		int n = info.length;	
         List<List<Integer>> graph = new ArrayList<>();
        	for (int i=0; i<n; i++) {
-            graph.add(new ArrayList<>());
-        } 
-       
-        for (int[] edge: edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph.get(u).add(v);
+           	graph.add(new ArrayList<>()); 
         }
         
+        for (int[] edge: edges) {
+            int parent = edge[0];
+            int child = edge[1];
+            graph.get(parent).add(child);
+        }
+      
         int start = 0;
         List<Integer> candidates = new ArrayList<>();
-       	candidates.add(start);
-        
+        candidates.add(start);
+       
         dfs(info, graph, start, 0, 0, candidates);
         
-        return result;
+   		return maxSheep; 
     }
-    public void dfs(int[] info, List<List<Integer>> graph, int curr, int sheep, int wolf, List<Integer> candidates)  {
-        if (info[curr] == 0) sheep++;
-        else wolf++; 
+   
+    public void dfs (int[] info, List<List<Integer>> graph, int curr, int sheep, int wolf, List<Integer> candidates) {
+       	if (info[curr] == 0) sheep++;
+        else wolf++;
         
         if (sheep <= wolf) return; 
-       
-        result = Math.max(result, sheep); 
+       	maxSheep = Math.max(maxSheep, sheep);
         
-        List<Integer> nextCandidates = new ArrayList<>(candidates); 
-        nextCandidates.remove(Integer.valueOf(curr));
-        
+        List<Integer> newCandidates = new ArrayList<>(candidates);
+        newCandidates.remove(Integer.valueOf(curr));
+      
         for (int child: graph.get(curr)) {
-    		nextCandidates.add(child);        
+            newCandidates.add(child);
         }
         
-        for (int next: nextCandidates) {
-           	dfs(info, graph, next, sheep, wolf, nextCandidates); 
+        for (int next: newCandidates) {
+            dfs(info, graph, next, sheep, wolf, newCandidates);
         }
     }
 }
