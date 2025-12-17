83589ad49aaf3766f375afb82d855932d92470a7
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-12-17 22:17:40 +0900

    [level 3] Title: 스타 수열, Time: 158.06 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.java
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -2,35 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] a) {
-        int answer = -1;
-        
         int n = a.length; 
         if (n < 2) return 0; 
         
         Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : a) {
-           	freq.put(num, freq.getOrDefault(num, 0) + 1); 
+        for (int num: a) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
         
-        int maxLen = 0;
+        int maxLen = 0; 
         for (int star: freq.keySet()) {
             if (freq.get(star) * 2 <= maxLen) continue;
             
             int i = 0;
-            int starCount = 0;
+            int starCount = 0; 
             while (i < n - 1) {
                 if ((a[i] == star || a[i + 1] == star) && a[i] != a[i + 1]) {
-                    i += 2;
+                    i += 2; 
                     starCount++;
                 } else {
                     i++;
                 }
+                
+                maxLen = Math.max(maxLen, starCount * 2);
             }
-            maxLen = Math.max(maxLen, starCount * 2);
+            
         }
-        
-        // System.out.println(freq);
-        
         return maxLen;
     }
 }
