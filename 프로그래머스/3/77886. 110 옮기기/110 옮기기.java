0f4a265395f9f65c12c045fa9c708a41be65e93c
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-12-20 15:33:01 +0900

    [level 3] Title: 110 옮기기, Time: 95.02 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -1,42 +1,37 @@
 class Solution {
     public String[] solution(String[] s) {
-        int n = s.length; 
-        String[] answer = new String[n]; 
-        
-        for (int i=0; i<n; i++) {
+        int n = s.length;  
+        String[] answer = new String[n];
+        for (int i = 0; i < n; i++) {
             answer[i] = move110(s[i]);
         }
         return answer;
     }
     public String move110(String str) {
-        StringBuilder sb = new StringBuilder();
-        int count110 = 0; 
+        StringBuilder sb = new StringBuilder();         
+        int count110 = 0;
         
         for (char ch: str.toCharArray()) {
             sb.append(ch);
-            
-            int len = sb.length();
+
+            int len = sb.length(); 
             if (len >= 3) {
-                if (sb.charAt(len - 3) == '1' && 
-                    sb.charAt(len - 2) == '1' && 
-                    sb.charAt(len - 1) == '0') {
+                if (sb.charAt(len - 3) == '1' && sb.charAt(len - 2) == '1' && sb.charAt(len - 1) == '0') {
                     sb.delete(len - 3, len); 
                     count110++;
                 }
             }
         }
         
-        String remaining = sb.toString();
-        int lastZeroIndex = remaining.lastIndexOf('0') + 1;
-        //System.out.println(remaining);
-        //System.out.println(lastZeroIndex);
+        String remaining = sb.toString(); 
+        // System.out.println(remaining);
+
+        int lastZeroIndex = remaining.lastIndexOf("0");
         
         StringBuilder result = new StringBuilder();
-        result.append(remaining.substring(0, lastZeroIndex));
-        for (int i=0; i<count110; i++) {
-            result.append("110");
-        }
-        result.append(remaining.substring(lastZeroIndex));
+        result.append(remaining.substring(0, lastZeroIndex + 1));
+        result.append("110".repeat(count110));
+        result.append(remaining.substring(lastZeroIndex + 1)); 
         return result.toString();
     }
 }
