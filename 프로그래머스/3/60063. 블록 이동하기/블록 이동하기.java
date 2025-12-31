3ae39d0b396a9b7143a4336baf2323f0f0aab069
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2025-12-31 14:13:04 +0900

    [level 3] Title: 블록 이동하기, Time: 39.64 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
@@ -11,17 +11,21 @@ class Solution {
         int x2, y2; 
         int dist;
         public Robot(int x1, int y1, int x2, int y2, int dist) {
-           	if (x1 > x2 || x1 == x2 && y1 > y2) {
-                this.x1 = x2; 
-               	this.y1 = y2; 
-                this.x2 = x1; 
-                this.y2 = y1;
-            } else {
-                this.x1 = x1; 
-               	this.y1 = y1; 
-                this.x2 = x2; 
-                this.y2 = y2;
-            }
+           	// if (x1 > x2 || x1 == x2 && y1 > y2) {
+           	// this.x1 = x2; 
+           	// this.y1 = y2; 
+           	// this.x2 = x1; 
+           	// this.y2 = y1;
+           	// } else {
+           	// this.x1 = x1; 
+           	// this.y1 = y1; 
+           	// this.x2 = x2; 
+           	// this.y2 = y2;
+           	// }
+            this.x1 = x1; 
+           	this.y1 = y1; 
+           	this.x2 = x2; 
+           	this.y2 = y2;
             this.dist = dist; 
         }
         public String key() {
