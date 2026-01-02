926bda6e56de7ee6a1b5eed06fddd3e528299465
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-01-02 17:00:31 +0900

    [level 3] Title: 카운트 다운, Time: 40.71 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,52 +1,43 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int target) {
         int[][] dp = new int[target + 1][2];
-        
+
         for (int i = 1; i <= target; i++) {
             dp[i][0] = Integer.MAX_VALUE;
         }
-       
-        for (int score = 1; score <= target; score++) {
-            for (int single = 1; single <= 20; single++) {
-                int prev = score - single;
-                if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
-                    update(dp, score, dp[prev][0] + 1, dp[prev][1] + 1);
-                }
+
+        for (int i = 1; i <= target; i++) { 
+            for (int s = 1; s <= 20; s++) {
+                update(dp, i, i - s, true);
+            }
+
+            for (int s = 1; s <= 20; s++) {
+                update(dp, i, i - s * 2, false);
             }
             
-            for (int num = 1; num <= 20; num++) {
-                int doubleScore = num * 2; 
-                int prev = score - doubleScore; 
-                if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
-                    update(dp, score, dp[prev][0] + 1, dp[prev][1]);
-                }
+            for (int s = 1; s <= 20; s++) {
+                update(dp, i, i - s * 3, false);
             }
-            
-            for (int num = 1; num <= 20; num++) {
-               	int tripleScore = num * 3; 
-                int prev = score - tripleScore; 
-                if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
-                    update(dp, score, dp[prev][0] + 1, dp[prev][1]);
-                }
-            }
-            
-            int num = 50; 
-            int prev = score - num; 
-            if (prev >= 0 && dp[prev][0] != Integer.MAX_VALUE) {
-                update(dp, score, dp[prev][0] + 1, dp[prev][1] + 1);
-            }
+
+            update(dp, i, i - 50, true);
         }
-        
-        
+
         return dp[target];
     }
-    
-    public void update(int[][] dp, int score, int total, int singleOrBull) {
-       	if (total < dp[score][0]) {
-            dp[score][0] = total;
-            dp[score][1] = singleOrBull;
-        } else if (total == dp[score][0] && dp[score][1] < singleOrBull) {
-            dp[score][1] = singleOrBull;
+
+    private void update(int[][] dp, int cur, int prev, boolean single) {
+        if (prev < 0 || dp[prev][0] == Integer.MAX_VALUE) return;
+
+        int dart = dp[prev][0] + 1;
+        int singleCnt = dp[prev][1] + (single ? 1 : 0);
+
+        if (dart < dp[cur][0]) {
+            dp[cur][0] = dart;
+            dp[cur][1] = singleCnt;
+        } else if (dart == dp[cur][0]) {
+            dp[cur][1] = Math.max(dp[cur][1], singleCnt);
         }
     }
 }
