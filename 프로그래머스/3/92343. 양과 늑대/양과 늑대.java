a506f1405574136a84d0109041e1dc4d5f87e76b
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-01-21 13:25:07 +0900

    [level 3] Title: 양과 늑대, Time: 4.25 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -2,46 +2,53 @@ import java.util.*;
 
 class Solution {
     int maxSheep = 0;
+    List<List<Integer>> graph; 
+    int[] info;
+    
     public int solution(int[] info, int[][] edges) {
+       	this.info = info;
+   		int n = info.length; 
         
-        int n = info.length; 
-        List<List<Integer>> graph = new ArrayList<>();
+        graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+           	graph.add(new ArrayList<>());
         }
         
         for (int[] edge: edges) {
             int u = edge[0];
-            int v = edge[1];
+            int v = edge[1]; 
             
             graph.get(u).add(v);
         }
-
-        int start = 0;
+      
+        int start = 0; 
+        int sheep = 0, wolf = 0;
         List<Integer> candidates = new ArrayList<>();
-        candidates.add(start); 
+        candidates.add(start);
         
-        dfs(info, graph, 0, 0, 0, candidates);
+        dfs(start, sheep, wolf, candidates);
         
         return maxSheep;
     }
     
-    public void dfs(int[] info, List<List<Integer>> graph, int curr, int sheep, int wolf, List<Integer> candidates) {
-        if (info[curr] == 0) sheep++; 
-        else wolf++; 
+    private void dfs(int curr, int sheep, int wolf, List<Integer> candidates) {
+        if (info[curr] == 0) sheep++; // 양일 때
+        else wolf++; // 늑대일 때
         
-        if (sheep <= wolf) return; 
-        maxSheep = Math.max(maxSheep, sheep); 
-        
-        List<Integer> newCandidates = new ArrayList<>(candidates); 
-        newCandidates.remove(Integer.valueOf(curr));
+        if (sheep <= wolf) // 종료 조건 
+            return; 
+            
+        maxSheep = Math.max(maxSheep, sheep); // 최대 양 개수 갱신 
+   
+      	List<Integer> newCandidates = new ArrayList<>(candidates);	
+       	newCandidates.remove(Integer.valueOf(curr));
         
         for (int next: graph.get(curr)) {
-            newCandidates.add(next);
+       		newCandidates.add(next);     
         }
         
         for (int child: newCandidates) {
-            dfs(info, graph, child, sheep, wolf, newCandidates);
+            dfs(child, sheep, wolf, newCandidates);
         }
     }
 }
