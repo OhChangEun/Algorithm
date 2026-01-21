e205bd1afb488935b050c1af25001f8bf9f42cd1
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-01-21 15:10:26 +0900

    [level 3] Title: 기둥과 보 설치, Time: 32.34 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -1,73 +1,96 @@
 import java.util.*;
 
 class Solution {
+    boolean[][] pillars; 
+    boolean[][] beams; 
+    
     int n;
-    boolean[][] pillars;
-    boolean[][] beams;
-    public int[][] solution(int n_, int[][] build_frame) {
-       	n = n_; 
-      
-        pillars = new boolean[n+1][n+1];
-        beams = new boolean[n+1][n+1];
-       
-        for (int[] cmd: build_frame) {
-            int x = cmd[0];
-            int y = cmd[1];
-            int type = cmd[2]; // 0: 기둥, 1: 보
-            int op = cmd[3]; // 0: 삭제, 1: 설치 
-            
-            if (op == 1) { // 설치 
-                if (canBuild(x, y, type)) {
-                    if (type == 0) pillars[x][y] = true; 
-                    else beams[x][y] = true;
-                } 
-            } else { // 삭제 
-                // 일단 삭제 
-                if (type == 0) pillars[x][y] = false;
-                else beams[x][y] = false;
+
+    public int[][] solution(int n, int[][] build_frame) {
+        this.n = n; 
+        pillars = new boolean[n + 1][n + 1];
+        beams = new boolean[n + 1][n + 1];
                 
-                if (!isValid()) {
-                    if (type == 0) pillars[x][y] = true;
-                    else beams[x][y] = true;
+        for (int[] frame: build_frame) {
+            int x = frame[0];
+            int y = frame[1];
+            int material = frame[2];
+            int op = frame[3];
+            
+            if (material == 0) { // 기둥 
+                if (op == 1) { // 설치
+                    if (canBuildPillar(y, x)) {
+                        pillars[y][x] = true;
+                    }
+                } else { // 삭제 
+                    pillars[y][x] = false; 
+                    if (!isValid()) { 
+                        pillars[y][x] = true; // 유효하지 않으면 원상복구
+                    }
+                }
+            } else { // 보 
+                if (op == 1) {
+                    if (canBuildBeam(y, x)) {
+                        beams[y][x] = true;
+                    }
+                } else {
+                    beams[y][x] = false; 
+                    if (!isValid()) { 
+                        beams[y][x] = true; // 유효하지 않으면 원상복구
+                    }
                 }
             }
         }
         
-        List<int[]> list = new ArrayList<>();
-        for (int i=0; i<=n; i++) {
-            for (int j=0; j<=n; j++) {
-            	if (pillars[i][j]) list.add(new int[] {i, j, 0});   
-            	if (beams[i][j]) list.add(new int[] {i, j, 1});   
+        List<int[]> result = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (pillars[i][j]) result.add(new int[] {j, i, 0});
+                if (beams[i][j]) result.add(new int[] {j, i, 1});
             }
         }
-        
-        list.sort((a,b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            if (a[1] != b[1]) return a[1] - b[1];
-            return a[2] - b[2];
+    
+        result.sort((a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0]; 
+            if (a[1] != b[1]) return a[1] - b[1]; 
+            return a[2] - a[2];
         });
         
-        return list.toArray(new int[list.size()][]);
+        int resSize = result.size();
+        int[][] answer = new int[resSize][3];
+        for (int i = 0; i < resSize; i++) {
+            answer[i][0] = result.get(i)[0];
+            answer[i][1] = result.get(i)[1];
+            answer[i][2] = result.get(i)[2];
+        }
+        
+        return answer;
     }
     
-    private boolean canBuild(int x, int y, int type) {
-        if (type == 0) { // 기둥일 때 
-            return (y == 0 // 바닥 위 
-                || beams[x][y] || (x > 0 && beams[x - 1][y]) // 보 위 
-                || (y > 0 && pillars[x][y-1])); // 기둥 위 	
-        } else { // 보일 때 
-            return (y > 0 && (pillars[x][y-1] || pillars[x + 1][y - 1])) // 기둥 위 
-                    || ((x > 0 && beams[x - 1][y]) && beams[x + 1][y]); // 보 위 
-        }
-    }
-                    
     private boolean isValid() {
-        for (int i=0; i<=n; i++) {
-            for (int j=0; j<=n; j++) {
-                if (pillars[i][j] && !canBuild(i, j, 0)) return false; 
-                if (beams[i][j] && !canBuild(i, j, 1)) return false; 
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (pillars[i][j] && !canBuildPillar(i, j)) return false; 
+                if (beams[i][j] && !canBuildBeam(i, j)) return false; 
             }
         }
+        
         return true;
-    }               
+    }
+    
+    
+    private boolean canBuildPillar(int y, int x) {
+        if (y == 0 || beams[y][x] || (x > 0 && beams[y][x - 1] || (y > 0 && pillars[y - 1][x]))) {
+            return true;
+        }
+        return false; 
+    }
+    
+    private boolean canBuildBeam(int y, int x) {
+        if ((y > 0 && pillars[y - 1][x] || pillars[y - 1][x + 1])|| ((x > 0 && beams[y][x - 1]) && (x + 1 <= n && beams[y][x + 1]))) {
+            return true;
+        }
+        return false; 
+    }
 }
