a4176855d5c9e5e561e955e2b0b6c3c4baf334b5
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-01-21 20:56:55 +0900

    [level 3] Title: 표 병합, Time: 13.97 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.java
+++ 프로그래머스/3/150366. 표 병합/표 병합.java
@@ -1,15 +1,18 @@
 import java.util.*;
 
 class Solution {
+    final int NUM = 50; 
+    final int SIZE = NUM * NUM;
     int[] parent;
+    String[] values;
+    
     public String[] solution(String[] commands) {
-        int size = 50 * 50; 
+        parent = new int[SIZE];
+        values = new String[SIZE];  
         
-        String[] values = new String[size];         
-        parent = new int[size];
-        for (int i = 0; i < size; i++) {
-            parent[i] = i; 
-        }        
+        for (int i = 0; i < SIZE; i++) {
+            parent[i] = i;
+        }
         
         List<String> result = new ArrayList<>();
         
@@ -18,46 +21,47 @@ class Solution {
             String op = parts[0];
             
             if (op.equals("UPDATE")) {
-                // UPDATE r c value
-                if (parts.length == 4) {
+                if (parts.length == 4) { // UPDATE r c value
                     int r = Integer.parseInt(parts[1]);
                     int c = Integer.parseInt(parts[2]);
                     String value = parts[3];
                     
                     int idx = getIdx(r, c);
                     int root = find(idx);
-                    values[root] = value;
-                } else { // UPDATE value1 value2
-                    String value1 = parts[1];
-                    String value2 = parts[2];
+                    values[root] = value; 
                     
-                    for (int i = 0; i < size; i++) {
-                        if (values[i] != null && values[i].equals(value1)) {
-                            values[i] = value2;
+                } else { // UPDATE value1 value2
+                    String from = parts[1];
+                    String to = parts[2]; 
+                    
+                    for (int i = 0; i < SIZE; i++) {
+                        if (values[i] != null && values[i].equals(from)) {
+                            values[i] = to;
                         }
                     }
                 }
+             
             } else if (op.equals("MERGE")) { // MERGE r1 c1 r2 c2
                 int r1 = Integer.parseInt(parts[1]);
                 int c1 = Integer.parseInt(parts[2]);
                 int r2 = Integer.parseInt(parts[3]);
                 int c2 = Integer.parseInt(parts[4]);
                 
-                int idx1 = getIdx(r1, c1);
-                int idx2 = getIdx(r2, c2);
+                int idx1 = getIdx(r1, c1); 
+                int idx2 = getIdx(r2, c2); 
                 
                 int root1 = find(idx1);
-                int root2 = find(idx2); 
+                int root2 = find(idx2);
                 
-                if (root1 == root2) continue;
+                if (root1 == root2) continue; 
                 
-                // root2만 값을 가지는 경우: root2가 부모 
                 if (values[root1] == null && values[root2] != null) {
                     parent[root1] = root2; 
-                } else { // root1이 값이 있는 경우와 root1과 root2 둘다 값을 가지는 경우: root1이 부모 
+                } else {
                     parent[root2] = root1;
                 }
-            } else if (op.equals("UNMERGE")) { // UNMERGE r c 
+            
+            } else if (op.equals("UNMERGE")) { // UNMERGE r c
                 int r = Integer.parseInt(parts[1]);
                 int c = Integer.parseInt(parts[2]);
                 
@@ -66,7 +70,7 @@ class Solution {
                 String saved = values[root];
                 
                 List<Integer> merged = new ArrayList<>();
-                for (int i = 0; i < size; i++) {
+                for (int i = 0; i < SIZE; i++) {
                     if (find(i) == root) {
                         merged.add(i);
                     }
@@ -82,26 +86,24 @@ class Solution {
                 int r = Integer.parseInt(parts[1]);
                 int c = Integer.parseInt(parts[2]);
                 
-                int idx = getIdx(r, c);
+                int idx = getIdx(r, c); 
                 int root = find(idx);
                 result.add(values[root] == null ? "EMPTY" : values[root]);
             }
-            //System.out.println(parts[0]);
         }
-        
+       
         return result.toArray(new String[0]);
     }
     
-    private int getIdx(int r, int c) {
-        int idx = (r - 1) * 50 + (c - 1);
+    private int getIdx(int row, int col) {
+        int idx = (row - 1) * NUM + (col - 1);
         return idx; 
     }
     
     private int find(int x) {
         if (parent[x] == x) {
             return x;
-        }
-        
+        } 
         return parent[x] = find(parent[x]);
     }
 }
