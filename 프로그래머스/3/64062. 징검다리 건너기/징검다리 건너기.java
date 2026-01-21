2b6d8013fe80f9c2f0b9457fb550d579c7507738
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-01-21 15:47:18 +0900

    [level 3] Title: 징검다리 건너기, Time: 66.00 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,37 +1,43 @@
 class Solution {
     public int solution(int[] stones, int k) {
-        int max = 0; 
-        for (int stone: stones) {
-            max = Math.max(max, stone);
-        }
         
-   		int answer = 0; 
-        int left = 1; 
-        int right = max;
+        int left = 0;
+        int right = 200_000_000; 
+        
+        int answer = 0;
         while (left <= right) {
             int mid = (left + right) / 2;
-            System.out.println(mid);
-            if (canCross(stones, mid, k)) {
-                answer = mid; 
+            
+            if (canGo(stones.clone(), mid, k)) {
                 left = mid + 1;
+                answer = mid; 
             } else {
                 right = mid - 1;
             }
         }
         
-        return answer;
+        return answer + 1;
     }
     
-    public boolean canCross(int[] stones, int mid, int k) {
-        int count = 0; 
-        for (int stone: stones) {
-            if (stone - mid < 0) {
-               count++;
-            } else {
-               count = 0;
-            }
-            if (count >= k) return false;
+    // 배열에 num을 다 빼고,
+    // 연속된 0이 k보다 작으면 넘어갈 수 있음 
+    private boolean canGo(int[] arr, int num, int k) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.max(arr[i] - num, 0); 
         }
-        return true;
+        
+        int left = 0; 
+        int maxLen = 0;
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] == 0) {
+                int len = right - left + 1;
+                maxLen = Math.max(maxLen, len);
+            } else {
+                left = right + 1;
+            }
+        }
+        
+        if (maxLen < k) return true;
+        return false;
     }
 }
