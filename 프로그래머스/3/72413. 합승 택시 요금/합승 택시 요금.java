8e151e88c061c84fb30923a6a1dc189f7091375b
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-01-22 21:42:09 +0900

    [level 3] Title: 합승 택시 요금, Time: 188.93 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,68 +1,76 @@
 import java.util.*;
 
 class Solution {
- 	class Node implements Comparable<Node> {
-        int vertex;
+    List<List<Node>> graph; 
+    int n;
+    
+    class Node implements Comparable<Node> {
+        int vertex; 
         int cost; 
-        public Node (int vertex, int cost) {
-            this.vertex = vertex;
-            this.cost = cost;
+        
+        public Node(int vertex, int cost) {
+            this.vertex = vertex; 
+            this.cost = cost; 
         }
+        
         @Override
-        public int compareTo (Node other) {
-            return this.cost - other.cost;
+        public int compareTo(Node other) {
+            return this.cost - other.cost; 
         }
-    } 
-  	public int[] dijkstra (List<List<Node>> graph, int n, int start) {
+    }
+
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        this.n = n; 
+        
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+       
+        for (int[] fare: fares) {
+            int u = fare[0];
+            int v = fare[1];
+            int cost = fare[2];
+            
+            graph.get(u).add(new Node(v, cost));
+            graph.get(v).add(new Node(u, cost));
+        }
+      
+        int minCost = Integer.MAX_VALUE;
+        for (int start = 1; start <= n; start++) {
+            int cost[] = dijkstra(start);
+            int totalCost = cost[s] + cost[a] + cost[b];
+            
+            minCost = Math.min(minCost, totalCost);
+        }
+        
+        return minCost;
+    }
+    
+    private int[] dijkstra(int start) {
         int[] dist = new int[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0; 
+        dist[start] = 0;
         
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(start, 0));
         
         while (!pq.isEmpty()) {
-            Node curr = pq.poll();
-            int currNode = curr.vertex; 
-            int currCost = curr.cost; 
-           	if (currCost > dist[currNode]) continue; 
+            Node curr = pq.poll(); 
+            int currNode = curr.vertex;
+            int currCost = curr.cost;
+           
+            if (currCost > dist[currNode]) continue;
             
             for (Node next: graph.get(currNode)) {
-                int newCost = dist[currNode] + next.cost; 
+                int newCost = dist[currNode] + next.cost;
                 if (newCost < dist[next.vertex]) {
                     dist[next.vertex] = newCost; 
                     pq.offer(new Node(next.vertex, newCost));
                 }
             }
         }
+        
         return dist;
-    } 
-    
-    
-    public int solution(int n, int s, int a, int b, int[][] fares) {
-   		List<List<Node>> graph = new ArrayList<>();
-        for (int i=0; i<=n; i++) {
-           	graph.add(new ArrayList<>()); 
-        }
-        
-        for (int[] fare: fares) {
-            int u = fare[0];
-            int v = fare[1];
-            int w = fare[2];
-            graph.get(u).add(new Node(v, w));
-            graph.get(v).add(new Node(u, w));
-        }
-        
-        int[] distS = dijkstra(graph, n, s);
-        int[] distA = dijkstra(graph, n, a);
-        int[] distB = dijkstra(graph, n, b);
-  
-        int minCost = Integer.MAX_VALUE;
-        for (int k=1; k<=n; k++) {
-            int totalCost = distS[k] + distA[k] + distB[k]; 
-           	minCost = Math.min(minCost, totalCost); 
-        }
-
-        return minCost;
     }
 }
