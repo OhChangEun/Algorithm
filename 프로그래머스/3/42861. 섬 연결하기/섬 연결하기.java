c0943d312fa2a362815c9dab5edd1afd796fbce1
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-01-23 22:23:25 +0900

    [level 3] Title: 섬 연결하기, Time: 0.95 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,41 +2,44 @@ import java.util.*;
 
 class Solution {
     int[] parent;
+    
     public int solution(int n, int[][] costs) {
-       	parent = new int[n];
-        for (int i=0; i<n; i++) {
+        int answer = 0;
+        
+        parent = new int[n];
+        for (int i = 1; i < n; i++) {
             parent[i] = i;
         }
-        // System.out.println(parent);
+        
         Arrays.sort(costs, (a, b) -> a[2] - b[2]);
         
-        int totalCost = 0; 
-        int edgeCount = 0;
         for (int[] cost: costs) {
-			int a = cost[0];
-			int b = cost[1];
-			int edgeCost = cost[2];
+            int u = cost[0];
+            int v = cost[1];
+            int c = cost[2]; 
             
-            if (find(a) != find(b)) {
-                union(a, b);
-                totalCost += edgeCost; 
-               	edgeCount++;
-                
-                if (edgeCount == n-1) break;
+            if (find(u) != find(v)) {
+                union(u, v);
+                answer += c;
             }
         }
         
-        return totalCost;
+        
+        return answer;
     }
-    public int find(int x) {
-        if (x == parent[x]) return x; 
+    
+    private int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        
         return parent[x] = find(parent[x]);
     }
-    public void union(int a, int b) {
+    
+    private void union(int a, int b) {
         int rootA = find(a);
         int rootB = find(b);
         
-        if (rootA < rootB) parent[rootB] = rootA;
-        else parent[rootA] = rootB;
+        parent[rootA] = rootB;
     }
 }
