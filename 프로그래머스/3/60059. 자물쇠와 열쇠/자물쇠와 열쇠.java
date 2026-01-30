da483f58cf3b4752c3144d56df20ae877ad1dc20
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-01-30 17:15:37 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 60.09 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,78 +1,84 @@
-import java.util.*;
 class Solution {
-    int M, N; 
-    int size; 
+    int n, m; 
+    int size;
+    
     public boolean solution(int[][] key, int[][] lock) {
-        M = key.length; 
-        N = lock.length; 
-       
-        size = N + 2 * (M - 1); 
-        int[][] newLock = new int[size][size]; 
+        m = key.length; 
+        n = lock.length;
         
-        for (int i = M - 1; i < N + M - 1; i++) {
-            for (int j = M - 1; j < N + M - 1; j++) {
-                newLock[i][j] = lock[i - M + 1][j - M + 1]; 
+        size = n + 2 * (m - 1);
+        
+        int[][] newLock = new int[size][size];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                newLock[i + m - 1][j + m - 1] = lock[i][j];
             }
         }
-        /* 
-        for (int i=0; i<size; i++) {
-            for (int j=0; j<size; j++) {
-                System.out.print(newLock[i][j] + " ");
-            }
-            System.out.println();
-        } */
         
-        boolean result = false;
-        for (int k = 0; k < 4; k++) {
+        for (int dir = 0; dir < 4; dir++) {
             key = rotate(key);
+            //print(key);
+            //System.out.println();
             
-            for (int i = 0; i < N + M - 1; i++) {
-                for (int j = 0; j < N + M - 1; j++) {
-                    if (check(newLock, key, i, j)) {
-                        result = true;
-                    }
+            for (int i = 0; i < n + m - 1; i++) {
+                for (int j = 0; j < n + m - 1; j++) {
+                    if (check(newLock, key, i, j)) 
+                        return true; 
                 }
             }
         }
-        
-        return result; 
+   
+        return false; 
     }
-    public boolean check(int[][] lock, int[][] key, int x, int y) {
-        // key 값에 1로 되어있는 값을 lock에 기록하고, 
-        // lock을 검사해서 해당 범위가 다 1이면 true, 아니면 false
-        int[][] temp = new int[size][size]; 
+    
+        
+    private boolean check(int[][] lock, int[][] key, int y, int x) {
+        int[][] temp = new int[size][size];
+        // newLock 복사본
         for (int i = 0; i < size; i++) {
-            temp[i] = Arrays.copyOf(lock[i], size); 
+            temp[i] = lock[i].clone();
         }
         
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < M; j++) {
-                temp[x + i][y + j] += key[i][j];
+        // newLock 복사본에 key에 맞게 1채워 넣기 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                temp[i + y][j + x] += key[i][j];
+            }
+        }
+    
+        if (isValid(temp)) return true;
+        return false; 
+    }
+    
+    private boolean isValid(int[][] lock) {
+        // print(lock);
+        for (int i = m - 1; i < n + m - 1; i++) {
+            for (int j = m - 1; j < n + m - 1; j++) {
+                if (lock[i][j] != 1) 
+                    return false; 
             }
         }
         
-        return canSolve(temp);
+        return true; 
     }
     
-    public boolean canSolve(int[][] lock) {
-        boolean result = true;
-        for (int i = M - 1; i < N + M - 1; i++) {
-            for (int j = M - 1; j < N + M - 1; j++) {
-                if (lock[i][j] != 1) {
-                    result = false; 
-                } 
+    private void print(int[][] arr) {
+        for (int[] row: arr) {
+            for (int num: row) {
+                System.out.print(num + " ");
             }
+            System.out.println();
         }
-        return result;
+        
     }
     
-    public int[][] rotate(int[][] field) {
-        int[][] result = new int[M][M];
-        for (int i = 0; i < M; i++) {
-           for (int j = 0; j < M; j++) {
-                result[j][M - i - 1] = field[i][j]; 
+    private int[][] rotate(int[][] key) {
+        int[][] rotatedKey = new int[m][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                rotatedKey[j][m - i - 1] = key[i][j];
             }
         }
-        return result; 
+        return rotatedKey;
     }
 }
