bd47d080a435bc018de9f0209c337202ca6dab6f
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-01 18:13:23 +0900

    [level 2] Title: 주차 요금 계산, Time: 11.71 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,80 +2,65 @@ import java.util.*;
 
 class Solution {
     class Car {
-        ArrayDeque<Integer> stack;
-        int totalTime; 
-        int totalFee; 
-        
-        public Car() {
-            stack = new ArrayDeque<>();
-            totalTime = 0; 
-            totalFee = 0;
-        }
+        int inTime = -1;
+        int totalTime = 0;
     }
         
-    public int[] solution(int[] fees, String[] records) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
+    public int[] solution(int[] fees, String[] records) {                   
+        int baseTime = fees[0]; // 기본 시간 
+        int baseFee = fees[1]; // 기본 요금  
+        int unitTime = fees[2]; // 단위 시간 
+        int unitPrice = fees[3]; // 단위 요금
+    
         // <차량 번호, 차 정보> 
         Map<Integer, Car> carMap = new HashMap<>();
-        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
         for (String record: records) {
             String[] parts = record.split(" ");
-            String timeStr = parts[0];
+            int time = toMinute(parts[0]); // hh:mm -> 분 형태로 변환
             int carNum = Integer.parseInt(parts[1]);
             String cmd = parts[2]; 
             
-            int time = toMinute(timeStr); // hh:mm -> 분 형태로 변환
-
             carMap.putIfAbsent(carNum, new Car());
             Car car = carMap.get(carNum);
+            
             if (cmd.equals("IN")) {
-                car.stack.push(time);
+                car.inTime = time; 
             } else if (cmd.equals("OUT")) {
-                int startTime = car.stack.pop();
-                car.totalTime += time - startTime; 
+                car.totalTime += time - car.inTime; 
+                car.inTime = -1; 
             }
         }
         
         // 출차 내역없는 차량 처리 
         for (int key: carMap.keySet()) {
             Car car = carMap.get(key);
-            if (!car.stack.isEmpty()) { // 출차 내역이 없다면 
+            if (car.inTime != -1) { // 출차 내역이 없다면 
                 int endTime = toMinute("23:59");
-                int startTime = car.stack.pop();
+                int startTime = car.inTime;
                 
                 car.totalTime += endTime - startTime; 
             }
-            
+            pq.offer(key);
             // System.out.println(key + ": " + car.totalTime);
         }
         
         // 모든 차량에 대해 주차 요금 계산 
-        for (int key: carMap.keySet()) {
-            pq.offer(key);
-            Car car = carMap.get(key);
-            
-            int baseTime = fees[0]; // 기본 시간 
-            int baseFee = fees[1]; // 기본 요금  
-            int unitTime = fees[2]; // 단위 시간 
-            int unitPrice = fees[3]; // 단위 요금
-            
-            int fee = 0;
-            if (car.totalTime <= baseTime) {
-                fee = baseFee; 
-            } else {
-                fee = baseFee + (car.totalTime - baseTime + unitTime - 1) / unitTime * unitPrice;
-            }
-            car.totalFee = fee; 
-        }
-        
         int[] result = new int[pq.size()];
         int idx = 0;
         while (!pq.isEmpty()) {
             int carNum = pq.poll();
-            int fee = carMap.get(carNum).totalFee;
+            Car car = carMap.get(carNum);
             
-            result[idx++] = fee;
+            int totalFee = 0;
+            if (car.totalTime <= baseTime) {
+                totalFee = baseFee; 
+            } else {
+                totalFee = baseFee + (car.totalTime - baseTime + unitTime - 1) / unitTime * unitPrice;
+            }
+            
+            result[idx++] = totalFee;
         }
         
         return result;
