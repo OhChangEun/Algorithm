80880330abe367a452d7994394adcbd073bc4f21
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 18:29:47 +0900

    [level 3] Title: 여행경로, Time: 0.53 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,36 +1,38 @@
 import java.util.*;
 
 class Solution {
-    final String startAirport = "ICN";
+    final String START = "ICN";
+    Map<String, PriorityQueue<String>> graph; 
     List<String> pathList; 
-    Map<String, PriorityQueue<String>> graph;
     
     public String[] solution(String[][] tickets) {
+        int n = tickets.length; 
         
         graph = new HashMap<>();
         for (String[] ticket: tickets) {
             String from = ticket[0];
-            String to = ticket[1];
+            String to = ticket[1]; 
             
-            graph.putIfAbsent(from, new PriorityQueue<>());
+            graph.putIfAbsent(from, new PriorityQueue<>()); 
             graph.get(from).add(to);
         }
         
-        pathList = new ArrayList<>(); 
-        dfs(startAirport);
+        pathList = new ArrayList<>();
+        dfs(START);
+       
+        Collections.reverse(pathList); // 경로 뒤집기 
         
-        Collections.reverse(pathList);
         return pathList.toArray(new String[0]);
     }
     
     private void dfs(String curr) {
+        
         PriorityQueue<String> pq = graph.get(curr);
-
         while (pq != null && !pq.isEmpty()) {
-            String next = pq.poll();
-            dfs(next);
+        	String next = pq.poll();
+        	dfs(next);    
         }
         
-        pathList.add(curr);      
+        pathList.add(curr); // 막다른 길에 다다르면 도착했다는 의미이므로 경로 역순으로 넣기   
     }
 }
