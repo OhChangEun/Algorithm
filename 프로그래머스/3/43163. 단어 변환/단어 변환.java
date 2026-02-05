9f2eb4878a796a917ca99b585ef42d518b5bd987
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 12:24:40 +0900

    [level 3] Title: 단어 변환, Time: 0.51 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,71 +1,88 @@
 import java.util.*;
 
 class Solution {
+    List<List<Integer>> graph; 
+    int[] dist; 
+    
     public int solution(String begin, String target, String[] words) {
-   		List<String> list = new ArrayList<>();
-		list.add(begin);	
-     
-        boolean hasTarget = false;
-        int targetIndex = -1;
-        for (int i=0; i<words.length; i++) {
-       		list.add(words[i]);
-            if (words[i].equals(target)) {
-                hasTarget = true;
-                targetIndex = i + 1;
-            }
-        }
-        if (!hasTarget) return 0;
-      
-        int size = list.size();
-        List<List<Integer>> graph = new ArrayList<>(); 
-        for (int i=0; i<size; i++) {
-           	graph.add(new ArrayList<>()); 
+        int n = words.length; 
+        
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
         
-        for (int i=0; i<size-1; i++) {
-           	for (int j=i+1; j<size; j++) {
-                if (isConvertible(list.get(i), list.get(j))) {
+        // 첫 단어 + 단어 배열
+        List<String> wordsList = new ArrayList<>();
+        wordsList.add(begin);
+        for (String word: words) {
+            wordsList.add(word);
+        }
+        
+        // 찾고자 하는게 없다면 return 
+        if (!wordsList.contains(target)) 
+            return 0;
+        
+        // 단어 간 연관관계 매핑 
+        int len = wordsList.size();
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+            if (isDifferent(wordsList.get(i), wordsList.get(j))) {
                     graph.get(i).add(j);
                     graph.get(j).add(i);
                 }
-            } 
+            }
         }
-       	System.out.println(graph);
-        int start = 0;
-        int result = bfs(start, targetIndex, graph);
+        
+        dist = new int[len];
+        int result = bfs(0, wordsList.indexOf(target));
+        
         return result;
     }
-   	public int bfs(int start, int targetIndex, List<List<Integer>> graph) {
+    
+    private int bfs(int start, int target) {
+        Arrays.fill(dist, -1);
+        dist[start] = 0;
+        
         Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[graph.size()];
-        int[] distance = new int[graph.size()];
+        queue.offer(start); 
         
-        visited[start] = true; 
-        queue.offer(start);
-        
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int curr = queue.poll();
-            if (curr == targetIndex) return distance[curr];
+            
+            if (curr == target) {
+                return dist[target];
+            }
             
             for (int next: graph.get(curr)) {
-            	if (!visited[next]) {
-                    visited[next] = true; 
-                    distance[next] = distance[curr] + 1; 
+                if (dist[next] == -1) {
+                    dist[next] = dist[curr] + 1; 
                     queue.offer(next);
-                }    
+                }
             }
         }
         
         return 0;
-    } 
+    }
     
-    
-    public boolean isConvertible(String a, String b) {
-        int diff = 0;
-        for (int i=0; i<a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) diff++;
-            if (diff > 1) return false;
+    private void print(List<String> list) {
+        for (String str: list) {
+            System.out.print(str + " ");
         }
-        return diff == 1;
+        System.out.println();
+    }
+    
+    private boolean isDifferent(String a, String b) {
+        // 1글자 차이인지 
+        int cnt = 0; 
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                cnt++;
+                
+                if (cnt >= 2) return false; 
+            }
+        }
+        
+        return true; 
     }
 }
