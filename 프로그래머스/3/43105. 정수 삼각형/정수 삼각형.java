cccadc154eb46a333a265f66aa047d5a0d608cd3
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 10:59:00 +0900

    [level 3] Title: 정수 삼각형, Time: 12.37 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,12 +1,33 @@
 class Solution {
     public int solution(int[][] triangle) {
-        int start = triangle.length - 2; 
-        for (int i=start; i>=0; i--) {
-            for (int j=0; j<=i; j++) {
-                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        
+        int len = triangle.length;
+        int[][] dp = new int[len][len + 1]; 
+        dp[0][1] = triangle[0][0]; 
+        // print(triangle);
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 1; j <= i + 1; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j - 1];
             }
         }
         
-        return triangle[0][0];
+        // print(dp);
+        
+        int result = 0;
+        for (int j = 1; j <= len; j++) {
+            result = Math.max(result, dp[len - 1][j]);
+        }
+        
+        return result;
+    }
+    
+    private void print(int[][] arr) {
+        for (int[] row: arr) {
+            for (int num: row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
     }
 }
