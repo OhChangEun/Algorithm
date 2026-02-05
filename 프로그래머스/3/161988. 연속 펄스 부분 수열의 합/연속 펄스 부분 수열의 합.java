c7ac0ccd566e651f6695bc6c3365be863e0cd90d
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 19:15:42 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 26.49 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,32 +1,45 @@
-import java.util.*;
-
 class Solution {
-    public long kadane(int[] arr) {
-        long currSum = 0;
-        long maxSum = arr[0];
-        
-        for (int num: arr) {
-            currSum = Math.max(num, num + currSum); 
-            maxSum = Math.max(maxSum, currSum);
-        }
-        return maxSum;
-    }
-        
     public long solution(int[] sequence) {
-   		int size = sequence.length;
-        int[] arr1 = Arrays.copyOf(sequence, size);
-        int[] arr2 = Arrays.copyOf(sequence, size);
-  
-        for (int i=0; i<size; i++) {
-            if (i % 2 == 0) 
-           		arr1[i] *= -1;
-            else 
-                arr2[i] *= -1;
+        int n = sequence.length; 
+        if (n == 1) {
+            return Math.abs(sequence[0]);
         }
         
-        long res1 = kadane(arr1);
-        long res2 = kadane(arr2);
+        int[] posArr = sequence.clone(); // 1, -1, 1, ... 을 곱하는 배열 
+        int[] negArr = sequence.clone(); // -1, 1, -1, ... 을 곱하는 배열 
+       
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) { // 짝수 인덱스에 대해 
+                posArr[i] *= -1;
+            } else {
+                negArr[i] *= -1;
+            }
+        }
+                
+        long maxResPos = getMaxValue(posArr);
+        long maxResNeg = getMaxValue(negArr);
         
-        return Math.max(res1, res2);
+        return Math.max(maxResPos, maxResNeg);
+    }
+    
+    private void print(int[] arr) {
+        for (int num: arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+    
+    private long getMaxValue(int[] arr) {
+        int arrSize = arr.length; 
+        
+        long maxRes = 0;
+        long[] dp = new long[arrSize]; 
+        dp[0] = arr[0];
+        for (int i = 1; i < arrSize; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            maxRes = Math.max(maxRes, dp[i]);
+        }
+        
+        return maxRes;
     }
 }
