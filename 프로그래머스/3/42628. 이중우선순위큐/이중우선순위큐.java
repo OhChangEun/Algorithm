eb7a62cb42eb340dcdfbc40811d098bfb84fa454
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 11:31:05 +0900

    [level 3] Title: 이중우선순위큐, Time: 72.12 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -7,29 +7,25 @@ class Solution {
       
         for (String operation: operations) {
             String[] parts = operation.split(" ");
-			String op = parts[0];
-            int num = Integer.parseInt(parts[1]);
-           
-            if (op.equals("I")) {
-               	maxHeap.add(num); 
-               	minHeap.add(num); 
-            } else if (op.equals("D")) {
-               	if (minHeap.isEmpty()) continue;
+            
+            String op = parts[0];
+            if (op.equals("I")) { // 추가 
+                int num = Integer.parseInt(parts[1]);
+                maxHeap.offer(num);
+                minHeap.offer(num);
+            } else { // 삭제 
+                if (maxHeap.isEmpty()) continue; 
                 
-                if (num == 1) {
-                    int max = maxHeap.poll();
-                    minHeap.remove(max);
-                } else {
+                if (parts[1].startsWith("-")) {
                     int min = minHeap.poll();
                     maxHeap.remove(min);
+                } else {
+                    int max = maxHeap.poll();
+                    minHeap.remove(max);
                 }
             }
         }
-        
-        if (minHeap.isEmpty() && maxHeap.isEmpty()) {
-            return new int[]{0, 0};
-        } else {
-            return new int[]{maxHeap.poll(), minHeap.poll()};
-        }
+      
+        return maxHeap.isEmpty() ? new int[] {0, 0} : new int[] {maxHeap.poll(), minHeap.poll()};
     }
 }
