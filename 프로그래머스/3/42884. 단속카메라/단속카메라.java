0c865abaae6d1d5127d6d428325af686e7e92501
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 13:54:50 +0900

    [level 3] Title: 단속카메라, Time: 8.72 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,25 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-   		Arrays.sort(routes, (a, b) -> a[1] - b[1]);
-      
-        /*
-       	for(int[] route: routes) {
-        	System.out.println(route[0] + ", " + route[1]);
-        } 
-        */
+        
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
        
-        int cameraCnt = 0;
-        int lastCameraPos = -30001;
+        int cnt = 0;
+        int prevCamera = -30001;
         for (int[] route: routes) {
-           	int start = route[0]; 
-           	int end = route[1]; 
-            if (lastCameraPos < start) {
-               	cameraCnt++;
-               	lastCameraPos = end; 
+            int start = route[0]; 
+            int end = route[1];
+            
+            if (prevCamera < start) {
+                cnt++;
+                prevCamera = end; 
             }
         }
         
-        return cameraCnt;
+        return cnt;
+    }
+    
+    private void print(int[][] routes) {
+        for (int[] row: routes) {
+            for (int num: row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
     }
 }
