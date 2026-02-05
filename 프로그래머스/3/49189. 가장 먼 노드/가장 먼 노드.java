eb3ce7341b87feb0dcb303bcb63bdb2b81e6420d
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 18:05:32 +0900

    [level 3] Title: 가장 먼 노드, Time: 45.37 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,57 +1,59 @@
 import java.util.*;
 
 class Solution {
+    int n;
+    List<List<Integer>> graph;
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-       
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i=0; i<=n; i++) {
+        this.n = n; 
+        
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
-        // System.out.println(graph);
+      
         for (int[] e: edge) {
             int u = e[0];
             int v = e[1];
+            
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
-        // System.out.println(graph);
-       
-        int start = 1;
-        int[] dist = bfs(n, start, graph);
-
-        int max = 0;
-        for (int d: dist) {
-            max = Math.max(d, max);
+        
+        int start = 1; 
+        int[] dists = bfs(start); 
+        
+        int maxDist = 0;
+        for (int dist: dists) {
+            maxDist = Math.max(maxDist, dist);
         }
         
-        int count = 0;
-        for (int d: dist) {
-            if (d == max) count++;
+        int cnt = 0; 
+        for (int dist: dists) {
+            if (dist == maxDist) cnt++;
         }
-       
-        return count;
+        
+        return cnt;
     }
-    public int[] bfs(int n, int start, List<List<Integer>> graph) {
+    
+    private int[] bfs(int start) {
         int[] dist = new int[n + 1];
-        Arrays.fill(dist, -1); 
+        Arrays.fill(dist, -1);
+        dist[start] = 0; 
         
         Queue<Integer> queue = new LinkedList<>();
-        dist[start] = 0;
         queue.offer(start);
         
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int curr = queue.poll();
-        
-            // System.out.println(curr);
+            
             for (int next: graph.get(curr)) {
                 if (dist[next] == -1) {
-                    dist[next] = dist[curr] + 1;
+                    dist[next] = dist[curr] + 1; 
                     queue.offer(next);
                 }
             }
         }
-       
+        
         return dist;
     }
 }
