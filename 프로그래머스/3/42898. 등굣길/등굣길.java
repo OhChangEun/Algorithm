2e2fcd4451981ace079beba15488ef37fa8a592a
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 13:27:23 +0900

    [level 3] Title: 등굣길, Time: 0.61 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,27 +1,35 @@
 class Solution {
+    final int MOD = 1_000_000_007;
+    
     public int solution(int m, int n, int[][] puddles) {
-   		int[][] dp = new int[n + 1][m + 1];
-        boolean[][] isPuddle = new boolean[n + 1][m + 1];
-		int MAX  = 1_000_000_007;
-        // 웅덩이 위치 초기화 
+        int[][] dp = new int[n + 1][m + 1];
+        
+        boolean[][] isPuddle = new boolean[n + 1][m + 1]; 
         for (int[] puddle: puddles) {
-            int x = puddle[0]; 
-            int y = puddle[1];
-            isPuddle[y][x] = true;
+            int x = puddle[0];
+            int y = puddle[1]; 
+            isPuddle[y][x] = true; 
         }
         
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=m; j++) {
-                if (i == 1 && j == 1) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-                
+        dp[1][1] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
                 if (!isPuddle[i][j]) {
-                   	dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MAX; 
+                	dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) % MOD;
                 }
             }
         }
-        return dp[n][m];
+        
+        // print(dp);
+        return dp[n][m] % MOD;
+    }
+    
+    private void print(int[][] arr) {
+        for (int[] row: arr) {
+            for (int num: row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
     }
 }
