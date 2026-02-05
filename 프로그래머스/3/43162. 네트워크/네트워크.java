014a3d25366fc357fa68bb016cf364a3fb80d253
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-05 11:16:03 +0900

    [level 3] Title: 네트워크, Time: 1.18 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,52 @@
 import java.util.*;
 
 class Solution {
-    boolean[] visited; 
-    int count = 0;
+    List<List<Integer>> graph; 
+    boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n]; 
-        for (int i=0; i<n; i++) {
-            if (!visited[i]) {
-                bfs(i, n, computers); 
-                count++;
+        
+        int len = computers.length;
+        graph = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                // 자기 자신을 제외하고 
+                if (i != j && computers[i][j] == 1) {
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }
             }
         }
-        return count;
+        
+        int networkCnt = 0;
+        visited = new boolean[len];
+        for (int i = 0; i < len; i++) {
+            if (!visited[i]) {
+                visited[i] = true; 
+                bfs(i);
+                networkCnt++;
+            }
+        }
+        
+        return networkCnt;
     }
-    private void bfs(int num, int n, int[][] computers) {
+    
+    private void bfs(int start) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(num);
-        visited[num] = true; 
+        queue.offer(start); 
         
         while (!queue.isEmpty()) {
             int curr = queue.poll();
-        
-            for (int i=0; i<n; i++) {
-                if (i!=curr && computers[curr][i] == 1 && !visited[i]) {
-                    queue.offer(i);
-                    visited[i] = true;
-                } 
+            
+            for (int next: graph.get(curr)) {
+                if (!visited[next]) {
+                    visited[next] = true; 
+                    queue.offer(next);
+                }
             }
         }
     }
