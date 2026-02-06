5f89c81aa385625d458675b887f080d0f53c9db7
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-06 11:50:06 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.22 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,51 +1,65 @@
 import java.util.*;
 
 class Solution {
-   	class Job {
-        int id; // 작업 번호 
-        int start; // 작업 시작 시간 
-        int length; // 작업 소요 시간
+    class Job implements Comparable<Job> { 
+        int idx; 
+        int requireTime; 
+        int runTime; 
         
-        public Job(int id, int start, int length) {
-            this.id = id;
-            this.start = start;
-            this.length = length;
+        public Job(int idx, int requireTime, int runTime) {
+            this.idx = idx;
+            this.requireTime = requireTime;
+            this.runTime = runTime;
         }
-    } 
-    
+        
+        @Override
+        public int compareTo(Job other) {
+            if (this.runTime != other.runTime) return this.runTime - other.runTime; 
+            if (this.requireTime != other.requireTime) return this.requireTime - other.requireTime; 
+            return this.idx - other.idx; 
+        }
+    }
     public int solution(int[][] jobs) {
-        int n = jobs.length;
-       
-        Arrays.sort(jobs, (a, b) -> {
-           if (a[0] == b[0]) return a[1] - b[1];
-           return a[0] - b[0];
-        });
-       
-        PriorityQueue<Job> pq = new PriorityQueue<>((a, b) -> {
-            if (a.length != b.length) return a.length - b.length;
-            if (a.start != b.start) return a.start - b.start;
-            return a.id - b.id;
-        });
+        int totalTime = 0;
         
-        int time = 0; // 현재 시각
-        int total = 0; // 총 반환 시간의 합 
-        int idx = 0; // 아직 넣지 않은 작업 인덱스 
-        
-        while (idx < n || !pq.isEmpty()) {
-           	while (idx < n && jobs[idx][0] <= time) {
-               	pq.offer(new Job(idx, jobs[idx][0], jobs[idx][1]));
-                idx++; // 다음 작업으로
-            } 
+        List<Job> jobList = new ArrayList<>();
+        for (int i = 0; i < jobs.length; i++) {
+            int requireTime = jobs[i][0];
+            int runTime = jobs[i][1];
             
+            totalTime += runTime;
+            jobList.add(new Job(i, requireTime, runTime));
+        }
+        
+        // 작업 요청 시간 순으로 리스트 정렬
+        Collections.sort(jobList, (a, b) -> a.requireTime - b.requireTime);
+
+        int result = 0;
+        
+        int now = 0; 
+        int idx = 0; 
+        int done = 0; 
+        PriorityQueue<Job> pq = new PriorityQueue<>(); // 준비 큐 
+        while (done < jobs.length) {
+            // 현재 시간에 맞는 요청시간을 가진 작업들은 다 pq에 넣기 
+            while (idx < jobs.length && jobList.get(idx).requireTime <= now) {
+                pq.offer(jobList.get(idx));
+                idx++;
+            }
+            
+            // 현재 시간에 있는 최상단 pq 빼기 -> 작업 시작 
             if (!pq.isEmpty()) {
-               	Job curr = pq.poll();
-                time += curr.length; // 현재 시간 + 작업 실행 후 종료 시간 
-                total += (time - curr.start); // 반환 시간 = 종료 시간 - 요청 시간
+                Job curr = pq.poll();
+                
+                now += curr.runTime; 
+                int turnaroundTime = now - curr.requireTime; 
+                result += turnaroundTime; 
+                done++;
             } else {
-                time = jobs[idx][0]; // 다음 작업 시작 시간 
+                now = jobList.get(idx).requireTime;
             }
         }
         
-        return total / n;
+        return result / jobs.length;
     }
 }
