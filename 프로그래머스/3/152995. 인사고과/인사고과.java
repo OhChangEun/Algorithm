1d92e319dacf39b1c41127b7caeed5615c19b0d4
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-06 15:20:19 +0900

    [level 3] Title: 인사고과, Time: 134.66 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,30 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] scores) {
-   		int[] myScore = Arrays.copyOf(scores[0], 2); 
-      
-        Arrays.sort(scores, (a, b) -> {
-          	if (a[0] == b[0]) return a[1] - b[1];
-            return b[0] - a[0];
-        });
+        int myAttitudeScore = scores[0][0];
+        int myPeerScore = scores[0][1];
         
-        int maxScore2 = scores[0][1];
-        for (int i=1; i<scores.length; i++) {
-			if (scores[i][1] < maxScore2) {
-                if (scores[i][0] == myScore[0] && scores[i][1] == myScore[1]) return -1;
-           		scores[i][0] = -1; 
-                scores[i][1] = -1;
+        Arrays.sort(scores, (a, b) -> {
+            if (a[0] != b[0]) return b[0] - a[0];
+            return a[1] - b[1];
+        });
+
+        int prevPeerScore = scores[0][1]; 
+        for (int i = 1; i < scores.length; i++) {
+            if (prevPeerScore > scores[i][1]) { // 두 점수다 작다면
+                if (isMe(myAttitudeScore, myPeerScore, scores[i][0], scores[i][1])) { // 완호가 작다면 
+                    return -1;
+                }
+                scores[i][0] = -1;
+                scores[i][1] = -1; 
             } else {
-               	maxScore2 = scores[i][1]; 
+                prevPeerScore = scores[i][1];
             }
         }
-       	
+        
         int rate = 1;
-       	for (int[] score: scores) {
-           	int myTotalScore = myScore[0] + myScore[1]; 
-           	int totalScore = score[0] + score[1]; 
-            if (myTotalScore < totalScore) rate++;
-        } 
+        for (int[] score: scores) {       
+           	int total = score[0] + score[1];
+            int myScore = myAttitudeScore + myPeerScore;
+            if (total > myScore) {
+                rate++;
+            }
+        }
+        
         return rate;
+    }
+    
+    private boolean isMe(int a, int b, int c, int d) {
+        if (a == c && b == d) {
+            return true;
+        } 
+        return false; 
     }
 }
