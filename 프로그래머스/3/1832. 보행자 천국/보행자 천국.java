bf96bbe13d97fccda1c6950dbac854096c4a6719
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-06 20:09:16 +0900

    [level 3] Title: 보행자 천국, Time: 116.48 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,35 +1,46 @@
 class Solution {
-    static final int MOD = 20170805;
-
+    int[][] map; 
+    int MOD = 20170805;
     public int solution(int m, int n, int[][] cityMap) {
+        map = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = cityMap[i - 1][j - 1];
+            }
+        }
+        
         int[][] right = new int[m + 1][n + 1];
         int[][] down = new int[m + 1][n + 1];
-
         right[1][1] = 1;
-        down[1][1] = 1;
-
+        down[1][1] = 1; 
+        
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i == 1 && j == 1) continue;
-
-                int info = cityMap[i - 1][j - 1];
-
-                if (info == 1) { // 막힘
-                    right[i][j] = 0;
-                    down[i][j] = 0;
-                } 
-                else if (info == 0) { // 자유
-               		int total = (right[i][j - 1] + down[i - 1][j]) % MOD;
-                    right[i][j] = total;
-                    down[i][j] = total;
-                } 
-                else { // 직진만 가능 (2)
-                    right[i][j] = right[i][j - 1];
-                    down[i][j] = down[i - 1][j];
+                
+                int info = map[i][j];
+                if (info == 1) continue; 
+                
+                if (info == 2) {
+                    right[i][j] = (right[i][j] + right[i][j - 1]) % MOD;
+                    down[i][j] = (down[i][j] + down[i - 1][j]) % MOD; 
+                } else if (info == 0) {
+                    right[i][j] = (right[i][j - 1] + down[i - 1][j]) % MOD;
+                    down[i][j] = (right[i][j - 1] + down[i - 1][j]) % MOD; 
                 }
             }
         }
-
+        
+        // print(right);
         return (right[m][n - 1] + down[m - 1][n]) % MOD;
+    }
+    
+    private void print(int[][] arr) {
+        for (int[] row: arr) {
+            for (int num: row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
     }
 }
