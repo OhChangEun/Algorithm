e66c116f65ef5319c3fca132de306dd464062dc9
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-06 12:27:33 +0900

    [level 3] Title: 거스름돈, Time: 13.00 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,12 +1,14 @@
 class Solution {
+    final int MOD = 1_000_000_007;
+    
     public int solution(int n, int[] money) {
-        int MOD = 1_000_000_007; 
         
         int[] dp = new int[n + 1]; 
         dp[0] = 1; 
-        for (int coin: money) {
+        
+		for (int coin: money) {
             for (int i = coin; i <= n; i++) {
-                dp[i] = (dp[i] + dp[i - coin]) % MOD;
+                dp[i] = (dp[i] + dp[i - coin]) % MOD; 
             }
         }
         
