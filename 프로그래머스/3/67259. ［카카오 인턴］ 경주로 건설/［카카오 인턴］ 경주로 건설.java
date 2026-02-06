3649eba9eedd20dbe4425888845cbf7301a9733e
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-06 10:23:37 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 5.08 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,68 +1,75 @@
 import java.util.*;
 
 class Solution {
-    class Node { 
-        int x, y, dir, cost; 
-        public Node (int x, int y, int dir, int cost) { 
-            this.x = x; 
-            this.y = y; 
-            this.dir = dir; 
-            this.cost = cost; 
+    final int UP = 0; // 상
+    final int DOWN = 1; // 하
+    final int LEFT = 2; // 좌 
+    final int RIGHT = 3; // 우 
+    
+    int n; 
+    int[] dy = { -1, 1, 0, 0 };
+    int[] dx = { 0, 0, -1, 1 };
+    
+    class Node {
+        int y, x; 
+        int dir; 
+        
+        public Node(int y, int x, int dir) {
+            this.y = y;
+            this.x = x;
+            this.dir = dir;
         }
-    } 
-    // 0: 상
-    // 1: 하
-    // 2: 좌
-    // 3: 우
-    int[] dx = new int[] {0, 0, -1, 1};
-    int[] dy = new int[] {-1, 1, 0, 0};
-    // 각 방향에 따른 최소값
-    int[][][] cost;
+    }
     
     public int solution(int[][] board) {
-        int n = board.length;
-        cost = new int[n][n][4]; 
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                Arrays.fill(cost[i][j], Integer.MAX_VALUE);
-            }
-        } 
+        n = board.length;
         
-        Queue<Node> queue = new LinkedList<>(); 
-        queue.offer(new Node(0, 0, 1, 0));
-        queue.offer(new Node(0, 0, 3, 0));
-       
-        // 오른쪽, 아래 방향으로 시작
-        cost[0][0][1] = 0; 
-        cost[0][0][3] = 0; 
-
-        int minCost = Integer.MAX_VALUE;
+        int result = bfs(board, 0, 0);
+        return result;
+    }
+    
+    private int bfs(int[][] board, int y, int x) {
+        int minCost = Integer.MAX_VALUE; 
+        
+        int[][][] costs = new int[n][n][4]; // y, x, 4방향
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Arrays.fill(costs[i][j], -1);
+            }
+        }
+        costs[y][x][DOWN] = 0; 
+        costs[y][x][RIGHT] = 0; 
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(y, x, DOWN));
+        queue.offer(new Node(y, x, RIGHT));
+        
         while (!queue.isEmpty()) {
             Node curr = queue.poll();
-            // 도착지에 도착하면 방향에 상관없이 x,y 좌표를 통해 최소 비용 구하기  
-            if (curr.x == n-1 && curr.y == n-1) {
-                minCost = Math.min(minCost, curr.cost);
+            int cy = curr.y;
+            int cx = curr.x; 
+            int cDir = curr.dir; 
+            
+            if (cy == n - 1 && cx == n - 1) {
+                int cost = costs[cy][cx][cDir];
+                minCost = Math.min(minCost, cost);
             }
-            for (int dir=0; dir<4; dir++) {
-                int nx = curr.x + dx[dir]; 
-                int ny = curr.y + dy[dir]; 
-               
-                if (nx < 0 || ny < 0 || nx > n-1 || ny > n-1) continue; // 막다른 길 
-                if (board[nx][ny] == 1) continue; // 벽
+            
+            for (int dir = 0; dir < 4; dir++) {
+                int ny = cy + dy[dir];
+                int nx = cx + dx[dir];
                 
-                int newCost = 0;
-                if (dir == curr.dir) {
-                    newCost = curr.cost + 100;
-                } else {
-                    newCost = curr.cost + 600; 
+                if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue; 
+                if (board[ny][nx] == 1) continue; 
+                
+                int newCost = costs[cy][cx][cDir] + (cDir == dir ? 100 : 600);
+                if (costs[ny][nx][dir] == -1 || costs[ny][nx][dir] > newCost) { 
+                    costs[ny][nx][dir] = newCost; 
+                    queue.offer(new Node(ny, nx, dir));
                 }
-               
-                if (newCost < cost[nx][ny][dir]) {
-                    cost[nx][ny][dir] = newCost;
-                    queue.offer(new Node(nx, ny, dir, newCost));
-                }
-            } 
+            }
         }
+        
         return minCost;
     }
 }
