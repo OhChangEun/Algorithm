e2c6b74ddac289c88babd93fc7ff7f1b91cd392c
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-06 15:35:03 +0900

    [level 3] Title: 순위, Time: 24.59 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,54 +1,62 @@
 import java.util.*;
 
 class Solution {
+    int n;
+    List<List<Integer>> winGraph;
+    List<List<Integer>> loseGraph;
+    
     public int solution(int n, int[][] results) {
-   		List<List<Integer>> winGraph = new ArrayList<>();	
-   		List<List<Integer>> loseGraph = new ArrayList<>();	
-      
-        for (int i=0; i<=n; i++) {
-           	winGraph.add(new ArrayList<>()); 
-           	loseGraph.add(new ArrayList<>()); 
+        this.n = n;
+        
+        winGraph = new ArrayList<>();
+        loseGraph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            winGraph.add(new ArrayList<>());
+            loseGraph.add(new ArrayList<>());
         }
         
         for (int[] result: results) {
-           	int u = result[0]; 
-           	int v = result[1]; 
-        	winGraph.get(u).add(v); 
-        	loseGraph.get(v).add(u); 
-        } 
-		
+            int u = result[0];
+            int v = result[1];
+            
+            winGraph.get(u).add(v);
+            loseGraph.get(v).add(u);
+        }
+        
         int result = 0;
-        for (int i=1; i<=n; i++) {
-            int winCount = bfs(n, winGraph, i);
-            int loseCount = bfs(n, loseGraph, i);
-           
-            int totalCount = winCount + loseCount;
-            if (totalCount == n - 1) 
+        for (int i = 1; i <= n; i++) {
+            int winCnt = bfs(winGraph, i);
+            int loseCnt = bfs(loseGraph, i);
+            
+            int totalCnt = winCnt + loseCnt;
+            if (totalCnt == n - 1) {
                 result++;
+            }
         }
         
         return result;
     }
-    public int bfs(int n, List<List<Integer>> graph, int start) {
-        Queue<Integer> queue = new LinkedList<>(); 
-   		boolean[] visited = new boolean[n + 1]; 
+    
+    private int bfs(List<List<Integer>> graph, int start) {
+        boolean[] visited = new boolean[n + 1];
+        visited[start] = true;
         
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
         queue.offer(start);
-   		visited[start] = true; 
- 
-        // bfs 시 방문하는 노드 수
-        int count = 0;
+        
+        int cnt = 0; 
         while (!queue.isEmpty()) {
             int curr = queue.poll();
+            cnt++;
+            
             for (int next: graph.get(curr)) {
                 if (!visited[next]) {
-                    queue.offer(next);
                     visited[next] = true;
-                    count++;
+                    queue.offer(next);
                 }
             }
         }
         
-        return count;
+        return cnt - 1; // 시작 노드 제외
     }
 }
