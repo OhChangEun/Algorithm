11468413c91ac0fafb788acd39531b7107037f60
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-06 19:01:07 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 17.21 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,52 +1,51 @@
 class Solution {
-    char[] moves = {'d', 'l', 'r', 'u'};
-    int[] dx = { 0, -1, 1, 0 };
-    int[] dy = { 1, 0, 0, -1 };
-    char[] result;
-    int width, height;
-    int targetY, targetX;
-    int moveNum;
+    char[] mapping = {'d', 'l', 'r', 'u'};
+    
+    int[] dy = {1, 0, 0, -1}; // 하, 좌, 우 상(d, l, r, u 순)
+    int[] dx = {0, -1, 1, 0};
+    
+    int n, m;
+    int k; 
+    int[][] map;
+    
+    String result;
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        result = new char[k];
-     
-        height = n;
-        width = m; 
-        targetY = r-1;
-        targetX = c-1;
-        moveNum = k;
+       	this.n = n;
+       	this.m = m;
+        this.k = k; 
+        map = new int[n][m];
         
-        int dist = Math.abs(r - x) + Math.abs(c - y);
-        if (k < dist || (k-dist) % 2 != 0) return "impossible";
-        
-        StringBuilder sb = new StringBuilder();
-        if (dfs(0, y-1, x-1)) {
-            for (char ch: result) {
-                sb.append(ch);    
-            }
-        	return sb.toString();
-        } else {
-            return "impossible";
-        }
+        boolean isValid = dfs(x - 1, y - 1, r - 1, c - 1, "", 0);
+
+        return isValid ? result : "impossible";
     }
     
-    private boolean dfs(int depth, int x, int y) {
-        int dist = Math.abs(targetX - x) + Math.abs(targetY - y);
-        if (dist > moveNum - depth) return false;
+    private boolean dfs(int y, int x, int r, int c, String str, int depth) {
+        if (depth > k) return false; // 이동한 거리가 k보다 크다면 더이상 못감 
         
-        if (depth == moveNum && x == targetX && y == targetY) {
-            return true; 
-        }
-        for (int i=0; i<4; i++) {
-           	int nx = x + dx[i]; 
-           	int ny = y + dy[i]; 
-            
-            if (nx < 0 || nx >= width || ny < 0 || ny >= height) continue; 
-            result[depth] = moves[i];
-            if (dfs(depth + 1, nx, ny)) {
+        int dist = Math.abs(y - r) + Math.abs(x - c); // 현재 위치부터 목적지까지 남은거리 
+        int left = k - depth; // k 위치까지 사용할 수 있는 걸음 
+        if (dist > left) return false; // 도달 못함
+        if ((dist - left) % 2 != 0) return false; // 
+        
+        if (depth == k) {
+            if (y == r && x == c) {
+                result = str; 
                 return true;
             }
+            return false; 
         }
         
-        return false; 
+        for (int dir = 0; dir < 4; dir++) {
+           	int ny = y + dy[dir];
+            int nx = x + dx[dir]; 
+            
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+            
+            if (dfs(ny, nx, r, c, str + mapping[dir], depth + 1)) return true;
+            
+        }
+        
+        return false;
     }
 }
