4d57a2b2fe56ec9602c7de226b9078d271b3e195
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-06 17:25:27 +0900

    [level 3] Title: 표 편집, Time: 112.80 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -2,60 +2,47 @@ import java.util.*;
 
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-  		int[] prev = new int[n];
+        int[] prev = new int[n];
         int[] next = new int[n];
         
-        for (int i=0; i<n; i++) {
-            prev[i] = i - 1;
+        for (int i = 0; i < n; i++) {
+            prev[i] = i - 1; 
             next[i] = i + 1;
         }
-        /*
-        for (int num: next) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
+        next[n - 1] = -1; 
         
-        for (int num: prev) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-      	*/ 
-        next[n-1] = -1;
-       
-        Stack<Integer> removed = new Stack<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
         for (String input: cmd) {
-       		char op = input.charAt(0); 
-            
-            if (op == 'D' || op == 'U') {
-           		int distance = Integer.parseInt(input.substring(2)); 
-            	for (int i=0; i<distance; i++) {
+            char op = input.charAt(0);
+            if (op == 'U' || op == 'D') {
+                int dx = Integer.parseInt(input.substring(2));
+                
+                for (int i = 0; i < dx; i++) {
                     k = (op == 'U') ? prev[k] : next[k];
-                }    
-            	// System.out.println(k);
+                }         
             } else if (op == 'C') {
-                removed.add(k);
+                stack.push(k);
+                
                 if (prev[k] != -1) next[prev[k]] = next[k];
                 if (next[k] != -1) prev[next[k]] = prev[k];
-             
-                if (next[k] != -1) {
-                    k = next[k];
-                } else {
-                    k = prev[k];
-                }
+                
+                if (next[k] != -1) k = next[k]; 
+                else k = prev[k];
+                
             } else if (op == 'Z') {
-                int restore = removed.pop();
+                int restore = stack.pop();
+                
                 if (prev[restore] != -1) next[prev[restore]] = restore; 
-                if (next[restore] != -1) prev[next[restore]] = restore;
+                if (next[restore] != -1) prev[next[restore]] = restore; 
             }
         }
-      
+        
         StringBuilder sb = new StringBuilder("O".repeat(n));
-       	while (!removed.isEmpty()) {
-			int remove = removed.pop();
-            sb.setCharAt(remove, 'X');
-        } 
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            sb.setCharAt(idx, 'X');
+        }
         
         return sb.toString();
-        
     }
 }
