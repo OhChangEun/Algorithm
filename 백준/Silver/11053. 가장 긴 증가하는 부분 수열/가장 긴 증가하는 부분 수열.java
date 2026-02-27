ca052e8a6cb7585a9709a933eed0a05e9e390f8a
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-02-28 04:53:11 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,29 +1,47 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int n;
+    static int[] arr;
+    static int[] lis;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        n = Integer.parseInt(br.readLine());
 
+        arr = new int[n];
         String[] parts = br.readLine().split(" ");
-        for (int i=0; i<N; i++) {
-            A[i] = Integer.parseInt(parts[i]);
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(parts[i]);
         }
 
-        int max = 0;
-        int[] dp = new int[N];
-        for (int i=0; i<N; i++) {
-            dp[i] = 1;
-            for (int j=0; j<i; j++) {
-                if (A[j] < A[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        int len = 0;
+        lis = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = len - 1;
+            int target = len;
+
+            while (left <= right) {
+                int mid = (left + right) / 2;
+
+                if (arr[lis[mid]] >= arr[i]) {
+                    target = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
             }
-            max = Math.max(max, dp[i]);
+
+            lis[target] = i;
+            if (target == len) {
+                len++;
+            }
         }
 
-        System.out.println(max);
+        System.out.println(len);
     }
 }
