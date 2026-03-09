4009cd4efa999c8c239e9d34067096d3513b4824
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-03-10 01:23:24 +0900

    [level 2] Title: 과제 진행하기, Time: 6.54 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     class Task {
         String name; 
-        int duration; 
+        int duration;
         
         public Task(String name, int duration) {
             this.name = name;
@@ -13,38 +13,47 @@ class Solution {
     
     public String[] solution(String[][] plans) {
         Arrays.sort(plans, (a, b) -> a[1].compareTo(b[1]));
-
+        
         ArrayDeque<Task> stack = new ArrayDeque<>();
         List<String> result = new ArrayList<>();
         int prevTime = 0;
-
-        for (String[] plan : plans) {
+        
+        for (String[] plan: plans) {
+            String taskName = plan[0]; 
+            
             int startTime = toMinute(plan[1]);
             int duration = Integer.parseInt(plan[2]);
-            int gap = startTime - prevTime;
-
-            // gap 시간만큼 스택 소진
-            while (!stack.isEmpty() && gap > 0) {
-                Task top = stack.peek();
-                if (top.duration <= gap) {
-                    gap -= top.duration;
-                    result.add(stack.pop().name);
+           
+            // 작업할 수 있는 시간 
+            int spareTime = startTime - prevTime; 
+           
+            // 해야할 작업들이 있고, 작업할 시간도 남아있다면 
+            while (!stack.isEmpty() && spareTime > 0) {
+                Task waiting = stack.peek(); // 대기 중인 작업에 대해서 
+                if (waiting.duration <= spareTime) { // 작업 시간 내에 작업을 끝낼 수 있다면 
+                    spareTime -= waiting.duration; // 작업 진행하고 
+                    result.add(stack.pop().name); // 결과 출력 
                 } else {
-                    top.duration -= gap;
-                    gap = 0;
+                    waiting.duration -= spareTime; // 최대한 작업을 진행하고 대기 시간 기록
+                    spareTime = 0; // 더이상 작업진행 불가 
                 }
             }
-
-            stack.push(new Task(plan[0], duration));
-            prevTime = startTime;
+            
+            stack.push(new Task(taskName, duration));
+            prevTime = startTime; 
         }
-
-        // 남은 task 처리
+        
         while (!stack.isEmpty()) {
             result.add(stack.pop().name);
         }
-
-        return result.toArray(new String[0]);
+       
+        int n = result.size();
+        String[] answer = new String[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = result.get(i);
+        }
+        
+        return answer;
     }
     
     public int toMinute(String time) {
@@ -52,6 +61,6 @@ class Solution {
         int hour = Integer.parseInt(parts[0]);
         int minute = Integer.parseInt(parts[1]);
         
-        return hour * 60 + minute;
+        return hour * 60 + minute; 
     }
 }
