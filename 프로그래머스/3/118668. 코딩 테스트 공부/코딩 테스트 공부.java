54c5594ffbaf3ff5b955d8de6cab68c2935fc813
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-04-02 02:25:24 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 18.85 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -2,51 +2,60 @@ import java.util.*;
 
 class Solution {
     public int solution(int alp, int cop, int[][] problems) {
-        int maxAlgo = 0;
-        int maxCode = 0;
+        int maxAlp = alp;
+        int maxCop = cop; 
+        
         for (int[] p: problems) {
-            maxAlgo = Math.max(maxAlgo, p[0]);
-            maxCode = Math.max(maxCode, p[1]);
+            maxAlp = Math.max(maxAlp, p[0]);
+            maxCop = Math.max(maxCop, p[1]);
         }
         
-        alp = Math.min(maxAlgo, alp);
-        cop = Math.min(maxCode, cop);
-        
-        int[][] dp = new int[maxAlgo + 1][maxCode + 1];
-        for (int i = 0; i <= maxAlgo; i++) {
+        int[][] dp = new int[maxAlp + 1][maxCop + 1];
+        for (int i = 0; i <= maxAlp; i++) {
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
         
         dp[alp][cop] = 0;
-        for (int a = alp; a <= maxAlgo; a++) {
-            for (int c = cop; c <= maxCode; c++) {
-                if (dp[a][c] == Integer.MAX_VALUE) continue;
+        
+        for (int a = alp; a <= maxAlp; a++) {
+            for (int c = cop; c <= maxCop; c++) {
+                if (dp[a][c] == Integer.MAX_VALUE) continue; 
                 
-                if (a + 1 <= maxAlgo) {
+                if (a + 1 <= maxAlp) {
                     dp[a + 1][c] = Math.min(dp[a + 1][c], dp[a][c] + 1);
                 }
                 
-                if (c + 1 <= maxCode) {
+                if (c + 1 <= maxCop) {
                     dp[a][c + 1] = Math.min(dp[a][c + 1], dp[a][c] + 1);
                 }
                 
                 for (int[] p: problems) {
-                    int reqA = p[0];
-                    int reqC = p[1];
-                    int rewardA = p[2];
-                    int rewardC = p[3];
+                    int alpReq = p[0];
+                    int codReq = p[1];
+                    int alpRew = p[2];
+                    int codRew = p[3];
                     int cost = p[4];
                     
-                    if (a >= reqA && c >= reqC) {
-                        int na = Math.min(a + rewardA, maxAlgo);
-                        int nc = Math.min(c + rewardC, maxCode);
-                        dp[na][nc] = Math.min(dp[na][nc], dp[a][c] + cost);
+                    if (a >= alpReq && c >= codReq) {
+                        int nextAlp = Math.min(maxAlp, a + alpRew);
+                        int nextCod = Math.min(maxCop, c + codRew);
+                        dp[nextAlp][nextCod] = Math.min(dp[nextAlp][nextCod], dp[a][c] + cost);
                     }
                 }
-                
             }
         }
         
-        return dp[maxAlgo][maxCode];
+        //print(dp);
+        
+        return dp[maxAlp][maxCop];
+    }
+    
+    public void print(int[][] arr) {
+        for (int[] row: arr) {
+            for (int num: row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
     }
 }
