e3f18db6e4abdb5f5e6e0207550d5253727567d9
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-04-02 16:47:44 +0900

    [level 2] Title: 리코쳇 로봇, Time: 2.88 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,75 +1,82 @@
 import java.util.*;
 
 class Solution {
-   	int n, m; 
-   	int[][] dists; 
-    int sy, sx; 
-    int ey, ex; 
+   
+    char[][] map; 
     
-    int[] dx = { 0, 0, -1, 1 };
-    int[] dy = { 1, -1, 0, 0 };
+    int n, m;
+    int[] dy = {-1, 1, 0, 0};
+    int[] dx = {0, 0, -1, 1};
     
     public int solution(String[] board) {
-      	n = board.length;
+        n = board.length; 
         m = board[0].length();
         
-      	dists = new int[n][m];
-        for (int i = 0; i < n; i++){
-            Arrays.fill(dists[i], -1);
-        }
+        map = new char[n][m];
         
-       	for (int i = 0; i < n; i++) {
+        int startY = 0, startX = 0;
+        int endY = 0, endX = 0; 
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 char ch = board[i].charAt(j);
+                
+                map[i][j] = ch;
                 if (ch == 'R') {
-                    sy = i;
-                    sx = j;
+                    startY = i; 
+                    startX = j; 
                 } else if (ch == 'G') {
-                    ey = i;
-                    ex = j;
+                    endY = i; 
+                    endX = j; 
                 }
             }
         } 
         
-        int result = bfs(board);
-        
-        return result;
+        int result = bfs(startY, startX, endY, endX);
+        return result; 
     }
-    public int bfs(String[] board) {
-        Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] {sy, sx});
-        dists[sy][sx] = 0;
+    
+    public int bfs(int startY, int startX, int endY, int endX) {
+        int[][] dists = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dists[i], -1);
+        }
+        dists[startY][startX] = 0;
+
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {startY, startX});
         
         while (!queue.isEmpty()) {
             int[] curr = queue.poll();
             int cy = curr[0];
-            int cx = curr[1];
+            int cx = curr[1]; 
             
-            for (int i=0; i<4; i++) {
-                int nx = cx;
-                int ny = cy;
-                
-                if (cy == ey && cx == ex) {
-                    return dists[cy][cx];
-                }
+            if (cy == endY && cx == endX) {
+                return dists[cy][cx];
+            }
+            
+            for (int dir = 0; dir < 4; dir++) {
+                int ny = cy; 
+                int nx = cx; 
                 
                 while (true) {
-                    int tx = nx + dx[i];
-                    int ty = ny + dy[i];
+                    int ty = ny + dy[dir]; 
+                    int tx = nx + dx[dir]; 
                     
-                    if (tx < 0 || tx >= m || ty < 0 || ty >= n) break;
-                    if (board[ty].charAt(tx) == 'D') break;
-                    
-                    nx = tx;
-                    ny = ty;
+                    // 밖으로 나가는 경우나 벽을 만나는 경우
+                    if (ty < 0 || ty >= n || tx < 0 || tx >= m) break; 
+                    if (map[ty][tx] == 'D') break; 
+
+                    ny = ty; 
+                    nx = tx; 
                 }
-                
+                            
                 if (dists[ny][nx] == -1) {
                     dists[ny][nx] = dists[cy][cx] + 1;
-                    queue.offer(new int[]{ny ,nx});
+                    queue.offer(new int[] {ny, nx});
                 }
             }
         }
+        
         return -1;
     }
 }
