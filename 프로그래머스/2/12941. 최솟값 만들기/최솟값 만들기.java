613a9c78b4fed1aef6654a24c8ebb75e7922585f
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-06-23 15:20:23 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.90 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -4,14 +4,18 @@ class Solution
 {
     public int solution(int []A, int []B)
     {
+        int answer = 0;
+
         Arrays.sort(A);
         Arrays.sort(B);
-       
-        int total = 0;
-        for (int i=0; i<A.length; i++) {
-            total += A[i] * B[B.length - i - 1]; 
-        } 
 
-        return total;
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[B.length - i - 1];
+        }
+        return answer;
+    }
+    
+    private void print(Object o) {
+        System.out.println(o);
     }
 }
