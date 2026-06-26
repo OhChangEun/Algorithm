6b7cac974cddd28b533e74c4fa030b224d98608c
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-06-26 13:47:18 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.15 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,65 +1,69 @@
 import java.util.*;
 
 class Solution {
-    class Job implements Comparable<Job> { 
-        int idx; 
-        int requireTime; 
-        int runTime; 
+    class Task implements Comparable<Task>{
+        int idx;
+        int start; 
+        int duration; 
         
-        public Job(int idx, int requireTime, int runTime) {
+        public Task(int idx, int start, int duration) {
             this.idx = idx;
-            this.requireTime = requireTime;
-            this.runTime = runTime;
+            this.start = start; 
+            this.duration = duration; 
         }
         
         @Override
-        public int compareTo(Job other) {
-            if (this.runTime != other.runTime) return this.runTime - other.runTime; 
-            if (this.requireTime != other.requireTime) return this.requireTime - other.requireTime; 
-            return this.idx - other.idx; 
+        public int compareTo(Task other) {
+            if (this.duration != other.duration) 
+                return this.duration - other.duration;
+            if (this.start != other.start) 
+                return this.start - other.start;
+            return this.idx - other.idx;
         }
-    }
+    } 
+    
     public int solution(int[][] jobs) {
-        int totalTime = 0;
+        int n = jobs.length;  
         
-        List<Job> jobList = new ArrayList<>();
-        for (int i = 0; i < jobs.length; i++) {
-            int requireTime = jobs[i][0];
-            int runTime = jobs[i][1];
-            
-            totalTime += runTime;
-            jobList.add(new Job(i, requireTime, runTime));
+        List<Task> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int start = jobs[i][0];
+            int duration = jobs[i][1]; 
+        
+            list.add(new Task(i, start, duration));
         }
         
-        // 작업 요청 시간 순으로 리스트 정렬
-        Collections.sort(jobList, (a, b) -> a.requireTime - b.requireTime);
-
-        int result = 0;
+        list.sort((a, b) -> a.start - b.start);
         
-        int now = 0; 
-        int idx = 0; 
+        int turnarroundTime = 0;
+        int currTime = 0; 
         int done = 0; 
-        PriorityQueue<Job> pq = new PriorityQueue<>(); // 준비 큐 
-        while (done < jobs.length) {
-            // 현재 시간에 맞는 요청시간을 가진 작업들은 다 pq에 넣기 
-            while (idx < jobs.length && jobList.get(idx).requireTime <= now) {
-                pq.offer(jobList.get(idx));
+        int idx = 0;
+        PriorityQueue<Task> pq = new PriorityQueue<>();
+        while (done < n) {
+            // 현재 시간에 진행할 수 있는 모든 작업을 빼서 
+            while (idx < n && list.get(idx).start <= currTime) {
+                pq.offer(list.get(idx));
                 idx++;
             }
             
-            // 현재 시간에 있는 최상단 pq 빼기 -> 작업 시작 
+            // 그 작업 중 가장 소요시간이 짧은 작업 진행 
             if (!pq.isEmpty()) {
-                Job curr = pq.poll();
+                Task currTask = pq.poll();
+                currTime += currTask.duration; 
                 
-                now += curr.runTime; 
-                int turnaroundTime = now - curr.requireTime; 
-                result += turnaroundTime; 
+                turnarroundTime += currTime - currTask.start;
                 done++;
             } else {
-                now = jobList.get(idx).requireTime;
+                currTime = list.get(idx).start;
             }
         }
+         
         
-        return result / jobs.length;
+        return turnarroundTime / n;
+    }
+    
+    private void print(Object o) {
+        System.out.println(o);   
     }
 }
