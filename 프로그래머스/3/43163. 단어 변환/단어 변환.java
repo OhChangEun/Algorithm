3f89863134ac06fb26ff8f88747f29bb0d2985a5
OhChangEun <131477920+OhChangEun@users.noreply.github.com>
2026-06-28 23:44:31 +0900

    [level 3] Title: 단어 변환, Time: 0.31 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,62 +1,64 @@
 import java.util.*;
 
 class Solution {
-    List<List<Integer>> graph; 
-    int[] dist; 
+    List<List<Integer>> graph;
+    int n;
     
+    // hit -> hot, 
+    // hot -> dot -> dog -> log, cog -> 
     public int solution(String begin, String target, String[] words) {
-        int n = words.length; 
-        
+        n = words.length; 
         graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n + 1; i++) {
             graph.add(new ArrayList<>());
         }
-        
-        // 첫 단어 + 단어 배열
-        List<String> wordsList = new ArrayList<>();
-        wordsList.add(begin);
+      
+        List<String> wordList = new ArrayList<>();
+        wordList.add(begin);
         for (String word: words) {
-            wordsList.add(word);
+            wordList.add(word);
         }
         
-        // 찾고자 하는게 없다면 return 
-        if (!wordsList.contains(target)) 
+        if (!wordList.contains(target)) 
             return 0;
         
-        // 단어 간 연관관계 매핑 
-        int len = wordsList.size();
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
-            if (isDifferent(wordsList.get(i), wordsList.get(j))) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n + 1; j++) {
+                String from = wordList.get(i);
+                String to = wordList.get(j);
+                
+                if (valid(from, to)) {
                     graph.get(i).add(j);
                     graph.get(j).add(i);
-                }
-            }
+                } 
+            } 
         }
         
-        dist = new int[len];
-        int result = bfs(0, wordsList.indexOf(target));
+        // print(graph);
         
-        return result;
+        int min = bfs(0, wordList.indexOf(target));
+                   
+        return min;  
     }
     
-    private int bfs(int start, int target) {
-        Arrays.fill(dist, -1);
-        dist[start] = 0;
+    public int bfs(int start, int target) {
+        int[] dists = new int[n + 1]; 
+        Arrays.fill(dists, Integer.MAX_VALUE); 
+        dists[start] = 0;
         
-        Queue<Integer> queue = new LinkedList<>();
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
         queue.offer(start); 
         
         while (!queue.isEmpty()) {
             int curr = queue.poll();
             
             if (curr == target) {
-                return dist[target];
+                return dists[curr];
             }
             
             for (int next: graph.get(curr)) {
-                if (dist[next] == -1) {
-                    dist[next] = dist[curr] + 1; 
+                if (dists[next] == Integer.MAX_VALUE) {
+                    dists[next] = dists[curr] + 1;
                     queue.offer(next);
                 }
             }
@@ -65,24 +67,21 @@ class Solution {
         return 0;
     }
     
-    private void print(List<String> list) {
-        for (String str: list) {
-            System.out.print(str + " ");
-        }
-        System.out.println();
+    public void print(Object o) {
+        System.out.println(o);
     }
     
-    private boolean isDifferent(String a, String b) {
-        // 1글자 차이인지 
-        int cnt = 0; 
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) {
+    public boolean valid(String a, String b) {
+        int n = a.length();
+       
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (a.charAt(i) != b.charAt(i)) 
                 cnt++;
-                
-                if (cnt >= 2) return false; 
-            }
+            if (cnt > 1) return false; 
         }
         
-        return true; 
+        return true;
     }
+                 
 }
